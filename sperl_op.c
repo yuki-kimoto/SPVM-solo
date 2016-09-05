f1ee22fb08fd05d77427b5528cee53e240e78616
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-05 17:00:16 +0900

    cleanup op

--- sperl_op.c
+++ sperl_op.c
@@ -21,8 +21,10 @@ SPerl_OP* SPerl_newOP(I32 type, I32 flags) {
 
 SPerl_OP* SPerl_newBINOP(I32 type, I32 flags, SPerl_OP* first, SPerl_OP* last) {
         
+  SPerl_UNOP *unop;
+  SPerl_BINOP *binop;
+  
   if (!last) {
-    SPerl_UNOP *unop;
 
     if (!first)
       first = SPerl_newOP(SPerl_OP_STUB, 0);
@@ -41,35 +43,36 @@ SPerl_OP* SPerl_newBINOP(I32 type, I32 flags, SPerl_OP* first, SPerl_OP* last) {
 
     return (SPerl_OP *)unop;
   }
-
-  SPerl_BINOP *binop;
-
-  SPerl_NewOp(binop, 1, SPerl_BINOP);
-
-  if (!first)
-    first = SPerl_newOP(SPerl_OP_NULL, 0);
-
-  SPerl_OpTYPE_set(binop, type);
-  binop->op_first = first;
-  binop->op_flags = (U8)(flags | SPerl_OPf_KIDS);
-  if (!last) {
-    last = first;
-    binop->op_private = (U8)(1 | (flags >> 8));
-  }
   else {
-    binop->op_private = (U8)(2 | (flags >> 8));
-      SPerl_OpMORESIB_set(first, last);
+
+
+    SPerl_NewOp(binop, 1, SPerl_BINOP);
+
+    if (!first)
+      first = SPerl_newOP(SPerl_OP_NULL, 0);
+
+    SPerl_OpTYPE_set(binop, type);
+    binop->op_first = first;
+    binop->op_flags = (U8)(flags | SPerl_OPf_KIDS);
+    if (!last) {
+      last = first;
+      binop->op_private = (U8)(1 | (flags >> 8));
+    }
+    else {
+      binop->op_private = (U8)(2 | (flags >> 8));
+        SPerl_OpMORESIB_set(first, last);
+    }
+
+    if (!SPerl_OpHAS_SIBLING(last)) /* true unless weird syntax error */
+      SPerl_OpLASTSIB_set(last, (SPerl_OP*)binop);
+
+    binop->op_last = SPerl_OpSIBLING(binop->op_first);
+    if (binop->op_last)
+      SPerl_OpLASTSIB_set(binop->op_last, (SPerl_OP*)binop);
+
+    if (binop->op_next || binop->op_type != (SPerl_OPCODE)type)
+      return (SPerl_OP*)binop;
+
+    return (SPerl_OP *)binop;
   }
-
-  if (!SPerl_OpHAS_SIBLING(last)) /* true unless weird syntax error */
-    SPerl_OpLASTSIB_set(last, (SPerl_OP*)binop);
-
-  binop->op_last = SPerl_OpSIBLING(binop->op_first);
-  if (binop->op_last)
-    SPerl_OpLASTSIB_set(binop->op_last, (SPerl_OP*)binop);
-
-  if (binop->op_next || binop->op_type != (SPerl_OPCODE)type)
-    return (SPerl_OP*)binop;
-
-  return (SPerl_OP *)binop;
 }
