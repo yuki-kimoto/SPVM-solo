123e2e0eeffaeaf67c1bd4b7afed5748120a25e9
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-15 12:04:51 +0900

    cleanup

--- sperl.h
+++ sperl.h
@@ -1,34 +1,4 @@
 #ifndef SPERL_H
 #define SPERL_H
 
-/* bool is built-in for g++-2.6.3 and later, which might be used
-   for extensions.  <_G_config.h> defines _G_HAVE_BOOL, but we can't
-   be sure _G_config.h will be included before this file.  _G_config.h
-   also defines _G_HAVE_BOOL for both gcc and g++, but only g++
-   actually has bool.  Hence, _G_HAVE_BOOL is pretty useless for us.
-   g++ can be identified by __GNUG__.
-   Andy Dougherty	February 2000
-*/
-#ifdef __GNUG__		/* GNU g++ has bool built-in */
-# ifndef PERL_BOOL_AS_CHAR
-#  ifndef HAS_BOOL
-#    define HAS_BOOL 1
-#  endif
-# endif
-#endif
-
-#ifndef HAS_BOOL
-# ifdef bool
-#  undef bool
-# endif
-# define bool char
-# define HAS_BOOL 1
-#endif
-
-/* cast-to-bool.  A simple (bool) cast may not do the right thing: if bool is
- * defined as char for example, then the cast from int is
- * implementation-defined (bool)!!(cbool) in a ternary triggers a bug in xlc on
- * AIX */
-#define cBOOL(cbool) ((cbool) ? (bool)1 : (bool)0)
-
 #endif
