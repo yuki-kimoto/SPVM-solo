b4ebc154d0687648baafdaa0f75f581ea957cdfb
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-16 11:30:51 +0900

    add argument information

--- sperl.h
+++ sperl.h
@@ -9,6 +9,15 @@ typedef struct SPerl_field_info SPerl_FIELD_INFO;
 struct SPerl_field_info {
   int8_t* name;
   int16_t desc_flag;
+  int8_t* type;
+};
+
+// Argument information
+struct SPerl_argument_info;
+typedef struct SPerl_argument_info SPerl_ARGUMENT_INFO;
+struct SPerl_SIGNATURE_INFO {
+  int16_t desc_flag;
+  int8_t* type;
 };
 
 // Method information
@@ -17,6 +26,9 @@ typedef struct SPerl_method_info SPerl_METHOD_INFO;
 struct SPerl_METHOD_INFO {
   int8_t* name;
   int16_t desc_flag;
+  int8_t* return_type;
+  SPerl_ARGUMENT_INFO* argument_infos;
+  int32_t argument_count;
 };
 
 // Class information
@@ -26,8 +38,10 @@ struct SPerl_class_info {
   int8_t* name;
   int16_t desc_flag;
   int8_t* super_class_name;
-  SPerl_FIELD_INFO* fields;
-  SPerl_METHOD_INFO* methods;
+  SPerl_FIELD_INFO* field_infos;
+  int32_t field_count;
+  SPerl_METHOD_INFO* method_infos;
+  int32_t method_count;
 };
 
 #endif
