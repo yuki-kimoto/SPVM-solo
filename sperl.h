c4b7d3b0535280cad05dc9a77755d81cf7abbec5
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-16 10:30:52 +0900

    add filed info and method info

--- sperl.h
+++ sperl.h
@@ -3,10 +3,28 @@
 
 #include <stdint.h>
 
+// Field
+struct SPerl_field_info;
+typedef struct SPerl_field_info SPerl_FIELD_INFO;
+struct SPerl_field_info {
+  int8_t* name;
+};
+
+// Method
+struct SPerl_method_info;
+typedef struct SPerl_method_info SPerl_METHOD_INFO;
+struct SPerl_METHOD_INFO {
+  int8_t* name;
+};
+
 struct SPerl_class_info;
 typedef struct SPerl_class_info SPerl_CLASS_INFO;
 struct SPerl_class_info {
-  int8_t something;
+  int8_t* name;
+  int16_t attr_flag;
+  int8_t* super_class_name;
+  SPerl_FIELD_INFO* fields;
+  SPerl_METHOD_INFO* methods;
 };
 
 #endif
