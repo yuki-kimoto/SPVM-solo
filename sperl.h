8bcb57c5168963a910407047d7329a511253c87a
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-16 10:11:03 +0900

    add class_info class

--- sperl.h
+++ sperl.h
@@ -1,4 +1,12 @@
 #ifndef SPERL_H
 #define SPERL_H
 
+#include <stdint.h>
+
+struct SPerl_class_info;
+typedef struct SPerl_class_info SPerl_CLASS_INFO;
+struct SPerl_class_info {
+  int8_t something;
+};
+
 #endif
