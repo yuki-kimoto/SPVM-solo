b296bf60f0f6982bdbee5749588ec262c9f1051e
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-16 10:43:08 +0900

    add descripter to field and method

--- sperl.h
+++ sperl.h
@@ -3,25 +3,28 @@
 
 #include <stdint.h>
 
-// Field
+// Field information
 struct SPerl_field_info;
 typedef struct SPerl_field_info SPerl_FIELD_INFO;
 struct SPerl_field_info {
   int8_t* name;
+  int16_t desc_flag;
 };
 
-// Method
+// Method information
 struct SPerl_method_info;
 typedef struct SPerl_method_info SPerl_METHOD_INFO;
 struct SPerl_METHOD_INFO {
   int8_t* name;
+  int16_t desc_flag;
 };
 
+// Class information
 struct SPerl_class_info;
 typedef struct SPerl_class_info SPerl_CLASS_INFO;
 struct SPerl_class_info {
   int8_t* name;
-  int16_t attr_flag;
+  int16_t desc_flag;
   int8_t* super_class_name;
   SPerl_FIELD_INFO* fields;
   SPerl_METHOD_INFO* methods;
