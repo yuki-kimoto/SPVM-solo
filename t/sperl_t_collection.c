c0791a4fa5a4e130df48bd7f0ae478d7685a8a98
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-22 08:53:56 +0900

    add OK macro

--- t/sperl_t_collection.c
+++ t/sperl_t_collection.c
@@ -2,63 +2,47 @@
 
 #include "../sperl_collection.h"
 
+#define OK(condition) \
+  if (condition) {\
+    printf("OK\n");\
+  }\
+  else {\
+    printf("Not OK at line %d\n", __LINE__);\
+  }
+
 int main(int argc, char *argv[])
 {
+  // Hash - new
+  {
+    SPerl_HASH* hash = SPerl_HASH_new(10);
+    // capacity
+    OK(hash->capacity == 10);
+  }
+  
   // Array - new
   {
     SPerl_ARRAY* array = SPerl_ARRAY_new(10);
     
     // capacity
-    if (array->capacity == 10) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
+    OK(array->capacity == 10);
     
     // length default is 0
-    if (array->length == 0) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    
-    }
+    OK(array->length == 0);
     
     // First element is NULL
-    if (array->values[0] == NULL) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
+    OK(array->values[0] == NULL);
     
     // Last element is NULL
-    if (array->values[9] == NULL) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
+    OK(array->values[9] == NULL);
   }
     
   // Array - capacity default
   {
     SPerl_ARRAY* array = SPerl_ARRAY_new(0);
     
-    if (array->capacity == 128) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
+    OK(array->capacity == 128);
     
-    if (array->length == 0) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
+    OK(array->length == 0);
   }
 
   // Array - push
@@ -68,44 +52,19 @@ int main(int argc, char *argv[])
     // push long value at first
     SPerl_long value1 = 10;
     SPerl_ARRAY_push(array, &value1);
-    if (*(SPerl_long*)array->values[0] == 10) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK\n");
-    }
-    if (array->length == 1) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
+    OK(*(SPerl_long*)array->values[0] == 10);
+    OK(array->length == 1);
     
     // push long value next
     SPerl_long value2 = 15;
     SPerl_ARRAY_push(array, &value2);
-    if (*(SPerl_long*)array->values[1] == 15) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK\n");
-    }
-    if (array->length == 2) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
+    OK(*(SPerl_long*)array->values[1] == 15);
+    OK(array->length == 2);
     
     // push pointer value
     SPerl_char* value3 = "foo";
     SPerl_ARRAY_push(array, value3);
-    if (array->values[2] == value3) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
+    OK(array->values[2] == value3);
   }
 
   // Array - push capacity resize
@@ -116,45 +75,15 @@ int main(int argc, char *argv[])
     SPerl_long value2 = 2;
     SPerl_ARRAY_push(array, &value1);
     SPerl_ARRAY_push(array, &value2);
-    if (array->capacity == 2) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK\n", __LINE__);
-    }
+    OK(array->capacity == 2);
     
     SPerl_long value3 = 3;
     SPerl_ARRAY_push(array, &value3);
-    if (array->capacity == 4) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
-    if (array->length == 3) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
-    if (*(SPerl_long*)array->values[0] == 1) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
-    if (*(SPerl_long*)array->values[1] == 2) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
-    if (*(SPerl_long*)array->values[2] == 3) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
+    OK(array->capacity == 4);
+    OK(array->length == 3);
+    OK(*(SPerl_long*)array->values[0] == 1);
+    OK(*(SPerl_long*)array->values[1] == 2);
+    OK(*(SPerl_long*)array->values[2] == 3);
   }
 
   // Array - fetch
@@ -166,24 +95,9 @@ int main(int argc, char *argv[])
     SPerl_ARRAY_push(array, &value1);
     SPerl_ARRAY_push(array, &value2);
     
-    if (*(SPerl_long*)SPerl_ARRAY_fetch(array, 0) == 3) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
-    if (*(SPerl_long*)SPerl_ARRAY_fetch(array, 1) == 5) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
-    if (SPerl_ARRAY_fetch(array, 2) == NULL) {
-      printf("OK\n");
-    }
-    else {
-      printf("Not OK %d\n", __LINE__);
-    }
+    OK(*(SPerl_long*)SPerl_ARRAY_fetch(array, 0) == 3);
+    OK(*(SPerl_long*)SPerl_ARRAY_fetch(array, 1) == 5);
+    OK(SPerl_ARRAY_fetch(array, 2) == NULL);
   }
   
   return 1;
