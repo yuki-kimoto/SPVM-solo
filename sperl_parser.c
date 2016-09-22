6968e5b3425a6c07b825e03f5013d02c95382e3c
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-22 18:00:05 +0900

    init parser method_infos

--- sperl_parser.c
+++ sperl_parser.c
@@ -5,5 +5,7 @@ SPerl_yy_parser* SPerl_new_parser() {
   SPerl_yy_parser* parser = (SPerl_yy_parser*)malloc(sizeof(SPerl_yy_parser));
   memset(parser, 0, sizeof(SPerl_yy_parser));
   
+  parser->method_infos = SPerl_ARRAY_new(0);
+  
   return parser;
 }
