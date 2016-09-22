110c555891a06136a632fd2f32cc2e14008c8802
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-22 16:18:13 +0900

    fix main

--- sperl_main.c
+++ sperl_main.c
@@ -26,15 +26,6 @@ int main(int argc, char *argv[])
     return -1;
   }
   
-  // Test array
-  SPerl_ARRAY* array = SPerl_ARRAY_new(10);
-  SPerl_VALUE* value = malloc(sizeof(SPerl_VALUE));
-  memset(value, 0, sizeof(SPerl_VALUE));
-  SPerl_CLASS_INFO* class_info = malloc(sizeof(SPerl_CLASS_INFO));
-  memset(value, 0, sizeof(SPerl_CLASS_INFO));
-  value->uv.ptr_value = class_info;
-  SPerl_ARRAY_push(array, value);
-  
   /* initialize parser */
   SPerl_yy_parser* parser = malloc(sizeof(SPerl_yy_parser));
 
