a61309b0b726194a20ad1c456849b397be34897a
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-24 14:31:26 +0900

    improve output

--- sperl_method_info.c
+++ sperl_method_info.c
@@ -28,15 +28,12 @@ void SPerl_METHOD_INFO_dump_method_infos(SPerl_ARRAY* method_infos) {
       SPerl_char* desc_str = (SPerl_char*)malloc(sizeof(SPerl_char) * 100);
       SPerl_DESCRIPTER_to_str(desc_str, method_info->desc_flags);
       
-      printf(
-        "[%d]name => \"%s\", argument_count => %d, desc_flags => \"%s\", return_type => \"%s\", op_block => %x\n",
-        i,
-        method_info->name,
-        method_info->argument_count,
-        desc_str,
-        method_info->return_type,
-        method_info->op_block
-      );
+      printf("[%d]\n", i);
+      printf("  name => \"%s\"\n", method_info->name);
+      printf("  argument_count => %d\n", method_info->argument_count);
+      printf("  desc_flags => \"%s\"\n", desc_str);
+      printf("  treturn_type => \"%s\"\n", method_info->return_type);
+      printf("  op_block => %x\n", method_info->op_block);
       
       free(desc_str);
     }
