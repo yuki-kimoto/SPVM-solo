7f85ef470a1eacf51b9af1c3890d677126c2abbc
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-26 14:33:55 +0900

    add method_infos to class_info

--- sperl_class_info.h
+++ sperl_class_info.h
@@ -12,8 +12,7 @@ struct SPerl_class_info {
   SPerl_char desc_flags;
   SPerl_FIELD_INFO* field_infos;
   SPerl_int field_count;
-  SPerl_int method_base_pos;
-  SPerl_int method_count;
+  SPerl_ARRAY* method_infos;
   SPerl_OP* op_block;
 };
 
