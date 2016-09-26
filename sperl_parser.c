6f35177a780b880538a199ffe5062823108f851e
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-27 08:34:28 +0900

    improve type printf

--- sperl_parser.c
+++ sperl_parser.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
 #include "sperl_type.h"
 #include "sperl_parser.h"
@@ -34,7 +35,7 @@ void SPerl_PARSER_dump_class_infos(SPerl_yy_parser* parser) {
   for (i = 0; i < class_infos->length; i++) {
     SPerl_CLASS_INFO* class_info = (SPerl_CLASS_INFO*)SPerl_ARRAY_fetch(class_infos, i);
     
-    printf("class_info[%d]\n", i);
+    printf("class_info[" PRId64 "]\n", i);
     printf("  name => \"%s\"\n", class_info->name);
     printf("  op_block => %x\n", class_info->op_block);
     
@@ -44,7 +45,7 @@ void SPerl_PARSER_dump_class_infos(SPerl_yy_parser* parser) {
     printf("  method_infos\n");
     for (j = 0; j < method_infos->length; j++) {
       SPerl_METHOD_INFO* method_info = SPerl_ARRAY_fetch(method_infos, j);
-      printf("    method_info[%d]\n", j);
+      printf("    method_info[%" PRId32 "]\n", j);
       SPerl_PARSER_dump_method_info(method_info);
     }
     
@@ -52,7 +53,7 @@ void SPerl_PARSER_dump_class_infos(SPerl_yy_parser* parser) {
     SPerl_ARRAY* const_ops = class_info->const_ops;
     for (j = 0; j < const_ops->length; j++) {
       SPerl_OP* const_op = SPerl_ARRAY_fetch(const_ops, j);
-      printf("    const_op[%d]\n", j);
+      printf("    const_op[%" PRId32 "]\n", j);
       SPerl_PARSER_dump_const_op(const_op);
     }
 
@@ -61,34 +62,34 @@ void SPerl_PARSER_dump_class_infos(SPerl_yy_parser* parser) {
       SPerl_OP* const_op = SPerl_ARRAY_fetch(const_ops, j);
       SPerl_PARSER_dump_const_pool(class_info->const_pool, const_op);
     }
-    printf("  const_pool_size %d\n", class_info->const_pool_size);
-    printf("  const_pool_next_pos %d\n", class_info->const_pool_next_pos);
+    printf("  const_pool_size %" PRId32 "\n", class_info->const_pool_size);
+    printf("  const_pool_next_pos %" PRId32 "\n", class_info->const_pool_next_pos);
   }
 }
 
 void SPerl_PARSER_dump_const_pool(SPerl_char* const_pool, SPerl_OP* const_op) {
   SPerl_int type = const_op->type;
-  printf("    const_pool[%d]\n", const_op->const_pos);
+  printf("    const_pool[%" PRId32 "]\n", const_op->const_pos);
   printf("      %s", SPerl_op_name[type]);
   if (type == SPerl_OP_CONST) {
     switch(const_op->private) {
       case SPerl_OPp_CONST_BOOLEAN:
-        printf("      boolean %d\n", *(SPerl_int*)(const_pool + const_op->const_pos));
+        printf("      boolean %" PRId32 "\n", *(SPerl_int*)(const_pool + const_op->const_pos));
         break;
       case SPerl_OPp_CONST_CHAR:
         printf("      char %c\n", *(SPerl_int*)(const_pool + const_op->const_pos));
         break;
       case SPerl_OPp_CONST_BYTE:
-        printf("      byte %d\n", *(SPerl_int*)(const_pool + const_op->const_pos));
+        printf("      byte %" PRId32 "\n", *(SPerl_int*)(const_pool + const_op->const_pos));
         break;
       case SPerl_OPp_CONST_SHORT:
-        printf("      short %d\n", *(SPerl_int*)(const_pool + const_op->const_pos));
+        printf("      short %" PRId32 "\n", *(SPerl_int*)(const_pool + const_op->const_pos));
         break;
       case SPerl_OPp_CONST_INT:
-        printf("      int %d\n", *(SPerl_int*)(const_pool + const_op->const_pos));
+        printf("      int %" PRId32 "\n", *(SPerl_int*)(const_pool + const_op->const_pos));
         break;
       case SPerl_OPp_CONST_LONG:
-        printf("      long %ld\n", *(SPerl_long*)(const_pool + const_op->const_pos));
+        printf("      long %" PRId64 "\n", *(SPerl_long*)(const_pool + const_op->const_pos));
         break;
       case SPerl_OPp_CONST_FLOAT:
         printf("      float %f\n", *(SPerl_float*)(const_pool + const_op->const_pos));
@@ -109,22 +110,22 @@ void SPerl_PARSER_dump_const_op(SPerl_OP* const_op) {
   if (type == SPerl_OP_CONST) {
     switch(const_op->private) {
       case SPerl_OPp_CONST_BOOLEAN:
-        printf("      boolean %d\n", const_op->uv.boolean_value);
+        printf("      boolean %" PRId32 "\n", const_op->uv.boolean_value);
         break;
       case SPerl_OPp_CONST_CHAR:
         printf("      char %c\n", const_op->uv.char_value);
         break;
       case SPerl_OPp_CONST_BYTE:
-        printf("      byte %d\n", const_op->uv.byte_value);
+        printf("      byte %" PRId32 "\n", const_op->uv.byte_value);
         break;
       case SPerl_OPp_CONST_SHORT:
-        printf("      short %d\n", const_op->uv.short_value);
+        printf("      short %" PRId32 "\n", const_op->uv.short_value);
         break;
       case SPerl_OPp_CONST_INT:
-        printf("      int %d\n", const_op->uv.int_value);
+        printf("      int %" PRId32 "\n", const_op->uv.int_value);
         break;
       case SPerl_OPp_CONST_LONG:
-        printf("      long %ld\n", const_op->uv.long_value);
+        printf("      long %" PRId64 "\n", const_op->uv.long_value);
         break;
       case SPerl_OPp_CONST_FLOAT:
         printf("      float %f\n", const_op->uv.float_value);
@@ -145,14 +146,14 @@ void SPerl_PARSER_dump_method_info(SPerl_METHOD_INFO* method_info) {
     SPerl_DESCRIPTER_to_str(desc_str, method_info->desc_flags);
     
     printf("      name => \"%s\"\n", method_info->name);
-    printf("      argument_count => %d\n", method_info->argument_count);
+    printf("      argument_count => %" PRId32 "\n", method_info->argument_count);
     
     SPerl_int j;
     printf("      argument_infos\n");
     for (j = 0; j < method_info->argument_count; j++) {
       SPerl_ARGUMENT_INFO* argument_info
         = (SPerl_ARGUMENT_INFO*)SPerl_ARRAY_fetch(method_info->argument_infos, j);
-      printf("        argument_info[%d]\n", j);
+      printf("        argument_info[%" PRId32 "]\n", j);
       printf("        type => \"%s\"\n", argument_info->type);
       
       SPerl_char* desc_str = (SPerl_char*)malloc(sizeof(SPerl_char) * 100);
