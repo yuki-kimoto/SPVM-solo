60a3b52732dd1e10d6f5a05f371d596339586d65
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-09-27 09:51:53 +0900

    add descripter value complex

--- sperl_descripter.c
+++ sperl_descripter.c
@@ -10,6 +10,12 @@ SPerl_char SPerl_DESCRIPTER_get_flag(SPerl_char* desc_str) {
   else if (strcmp(desc_str, "static") == 0) {
     return SPerl_DESCRIPTER_STATIC;
   }
+  else if (strcmp(desc_str, "interface") == 0) {
+    return SPerl_DESCRIPTER_INTERFACE;
+  }
+  else if (strcmp(desc_str, "value") == 0) {
+    return SPerl_DESCRIPTER_VALUE;
+  }
   else {
     return 0;
   }
@@ -19,9 +25,11 @@ void SPerl_DESCRIPTER_to_str(SPerl_char* str, SPerl_char flags) {
   
   sprintf(
     (char *)str,
-    "%s %s",
+    "%s %s %s %s",
     (flags & SPerl_DESCRIPTER_CONST) ? "const" : "",
-    (flags & SPerl_DESCRIPTER_STATIC) ? "static" : ""
+    (flags & SPerl_DESCRIPTER_STATIC) ? "static" : "",
+    (flags & SPerl_DESCRIPTER_INTERFACE) ? "interface" : "",
+    (flags & SPerl_DESCRIPTER_VALUE) ? "value" : ""
   );
 }
 
--- sperl_descripter.h
+++ sperl_descripter.h
@@ -3,6 +3,8 @@
 
 #define SPerl_DESCRIPTER_CONST 1
 #define SPerl_DESCRIPTER_STATIC 2
+#define SPerl_DESCRIPTER_INTERFACE 4
+#define SPerl_DESCRIPTER_VALUE 8
 
 SPerl_char SPerl_DESCRIPTER_get_flag(SPerl_char* desc_str);
 void SPerl_DESCRIPTER_to_str(SPerl_char* str, SPerl_char flags);
