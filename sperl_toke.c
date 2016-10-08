36aae44df8d04654dc9d233bd715d7c6746ec622
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-10-08 18:03:57 +0900

    cleanup parser->bufptr

--- sperl_toke.c
+++ sperl_toke.c
@@ -49,13 +49,12 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
     parser->bufptr = src;
   }
   
-  SPerl_char* bufptr = parser->bufptr;
   enum SPerl_OP_EXPECT expect = parser->expect;
   parser->expect = SPerl_OP_EXPECT_NORMAL;
   
   while(1) {
     // Get current character
-    SPerl_char c = *bufptr;
+    SPerl_char c = *parser->bufptr;
     
     // line end
     switch(c) {
@@ -102,8 +101,8 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
               src[file_size] = '\0';
               
               parser->cur_src = src;
-              bufptr = src;
-              parser->bufptr = bufptr;
+              parser->bufptr = src;
+              
               break;
             }
           }
@@ -122,291 +121,291 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
       case ' ':
       case '\t':
 
-        bufptr++;
-        parser->bufptr = bufptr;
+        parser->bufptr++;
+        
         continue;
 
       case '\n':
-        bufptr++;
-        parser->bufptr = bufptr;
+        parser->bufptr++;
+        
         parser->cur_line++;
         continue;
       
       /* Addition */
       case '+':
-        bufptr++;
+        parser->bufptr++;
         
-        if (*bufptr == '+') {
-          bufptr++;
-          parser->bufptr = bufptr;
+        if (*parser->bufptr == '+') {
+          parser->bufptr++;
+          
           SPerl_yylvalp->ival = SPerl_OP_INC;
           return INCOP;
         }
-        else if (*bufptr == '=') {
-          bufptr++;
-          parser->bufptr = bufptr;
+        else if (*parser->bufptr == '=') {
+          parser->bufptr++;
+          
           SPerl_yylvalp->ival = SPerl_OP_ADD;
           return ASSIGNOP;
         }
         else {
-          parser->bufptr = bufptr;
+          
           
           return '+';
         }
       
       /* Subtract */
       case '-':
-        bufptr++;
+        parser->bufptr++;
         
-        if (*bufptr == '>') {
-          bufptr++;
-          parser->bufptr = bufptr;
+        if (*parser->bufptr == '>') {
+          parser->bufptr++;
+          
           parser->expect = SPerl_OP_EXPECT_WORD;
           return ARROW;
         }
-        else if (*bufptr == '-') {
-          bufptr++;
-          parser->bufptr = bufptr;
+        else if (*parser->bufptr == '-') {
+          parser->bufptr++;
+          
           SPerl_yylvalp->ival = SPerl_OP_DEC;
           return DECOP;
         }
-        else if (*bufptr == '=') {
-          bufptr++;
-          parser->bufptr = bufptr;
+        else if (*parser->bufptr == '=') {
+          parser->bufptr++;
+          
           SPerl_yylvalp->ival = SPerl_OP_SUBTRACT;
           return ASSIGNOP;
         }
         else {
-          parser->bufptr = bufptr;
+          
 
           return '-';
         }
       /* Multiply */
       case '*':
-        bufptr++;
-        if (*bufptr == '=') {
-          bufptr++;
-          parser->bufptr = bufptr;
+        parser->bufptr++;
+        if (*parser->bufptr == '=') {
+          parser->bufptr++;
+          
           SPerl_yylvalp->ival = SPerl_OP_MULTIPLY;
           return ASSIGNOP;
         }
         else {
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_MULTIPLY;
           return MULOP;
         }
       
       /* Divide */
       case '/':
-        bufptr++;
-        if (*bufptr == '=') {
-          bufptr++;
-          parser->bufptr = bufptr;
+        parser->bufptr++;
+        if (*parser->bufptr == '=') {
+          parser->bufptr++;
+          
           SPerl_yylvalp->ival = SPerl_OP_DIVIDE;
           return ASSIGNOP;
         }
         else {
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_DIVIDE;
           return MULOP;
         }
 
       case '%':
-        bufptr++;
-        if (*bufptr == '=') {
-          bufptr++;
-          parser->bufptr = bufptr;
+        parser->bufptr++;
+        if (*parser->bufptr == '=') {
+          parser->bufptr++;
+          
           SPerl_yylvalp->ival = SPerl_OP_MODULO;
           return ASSIGNOP;
         }
         else {
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_MODULO;
           return MULOP;
         }
 
       case '^':
-        bufptr++;
-        if (*bufptr == '=') {
-          bufptr++;
-          parser->bufptr = bufptr;
+        parser->bufptr++;
+        if (*parser->bufptr == '=') {
+          parser->bufptr++;
+          
           SPerl_yylvalp->ival = SPerl_OP_BIT_XOR;
           return ASSIGNOP;
         }
         else {
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_BIT_XOR;
           return MULOP;
         }
             
       case '|':
-        bufptr++;
+        parser->bufptr++;
         /* Or */
-        if (*bufptr == '|') {
-          bufptr++;
-          if (*bufptr == '=') {
-            bufptr++;
-            parser->bufptr = bufptr;
+        if (*parser->bufptr == '|') {
+          parser->bufptr++;
+          if (*parser->bufptr == '=') {
+            parser->bufptr++;
+            
             SPerl_yylvalp->ival = SPerl_OP_OR;
             return ASSIGNOP;
           }
           else {
-            parser->bufptr = bufptr;
+            
             SPerl_yylvalp->ival = SPerl_OP_OR;
             return OROP;
           }
         }
         /* Bit or */
         else {
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_BIT_OR;
           return BITOROP;
         }
 
       case '&':
-        bufptr++;
+        parser->bufptr++;
         /* Or */
-        if (*bufptr == '&') {
-          bufptr++;
-          if (*bufptr == '=') {
-            bufptr++;
-            parser->bufptr = bufptr;
+        if (*parser->bufptr == '&') {
+          parser->bufptr++;
+          if (*parser->bufptr == '=') {
+            parser->bufptr++;
+            
             SPerl_yylvalp->ival = SPerl_OP_AND;
             return ASSIGNOP;
           }
           else {
-            parser->bufptr = bufptr;
+            
             SPerl_yylvalp->ival = SPerl_OP_AND;
             return ANDOP;
           }
         }
         /* Bit and */
         else {
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_BIT_AND;
           return BITANDOP;
         }
       
       /* Comment */
       case '#':
-        bufptr++;
+        parser->bufptr++;
         while(1) {
-          if (*bufptr == '\r' || *bufptr == '\n' || *bufptr == '\0') {
+          if (*parser->bufptr == '\r' || *parser->bufptr == '\n' || *parser->bufptr == '\0') {
             break;
           }
-          bufptr++;
+          parser->bufptr++;
         }
-        parser->bufptr = bufptr;
+        
         continue;
       
       case '=':
-        bufptr++;
+        parser->bufptr++;
         
         /* == */
-        if (*bufptr == '=') {
-          bufptr++;
+        if (*parser->bufptr == '=') {
+          parser->bufptr++;
           
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_EQ;
           return RELOP;
         }
         /* = */
         else {
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = 0;
           return ASSIGNOP;
         }
         
       case '<':
-        bufptr++;
+        parser->bufptr++;
         
-        if (*bufptr == '<') {
-          bufptr++;
+        if (*parser->bufptr == '<') {
+          parser->bufptr++;
           
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_LEFT_SHIFT;
           return SHIFTOP;
         }
         /* <= */
-        else if (*bufptr == '=') {
-          bufptr++;
+        else if (*parser->bufptr == '=') {
+          parser->bufptr++;
           
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_LE;
           return RELOP;
         }
         /* < */
         else {
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_LT;
           return RELOP;
         }
       
       case '>':
-        bufptr++;
+        parser->bufptr++;
         
-        if (*bufptr == '>') {
-          bufptr++;
+        if (*parser->bufptr == '>') {
+          parser->bufptr++;
           
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_RIGHT_SHIFT;
           return SHIFTOP;
         }
         /* >= */
-        else if (*bufptr == '=') {
-          bufptr++;
+        else if (*parser->bufptr == '=') {
+          parser->bufptr++;
           
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_GE;
           return RELOP;
         }
         /* < */
         else {
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_GT;
           return RELOP;
         }
       case '!':
-        bufptr++;
+        parser->bufptr++;
         
-        if (*bufptr == '=') {
-          bufptr++;
-          parser->bufptr = bufptr;
+        if (*parser->bufptr == '=') {
+          parser->bufptr++;
+          
           SPerl_yylvalp->ival = SPerl_OP_NE;
           return RELOP;
         }
         else {
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->ival = SPerl_OP_NOT;
           return NOTOP;
         }
         
       case '~':
-        bufptr++;
+        parser->bufptr++;
         
-        parser->bufptr = bufptr;
+        
         SPerl_yylvalp->ival = SPerl_OP_COMPLEMENT;
         return '~';
       
       case '\'': {
-        bufptr++;
+        parser->bufptr++;
         
         /* Save current position */
-        SPerl_char* cur_token_ptr = bufptr;
+        SPerl_char* cur_token_ptr = parser->bufptr;
         
         SPerl_char ch;
-        if (*bufptr == '\'') {
+        if (*parser->bufptr == '\'') {
           ch = '\0';
-          bufptr++;
+          parser->bufptr++;
         }
         else {
-          ch = *bufptr;
-          bufptr++;
-          if (*bufptr != '\'') {
+          ch = *parser->bufptr;
+          parser->bufptr++;
+          if (*parser->bufptr != '\'') {
             fprintf(stderr, "syntax error: string don't finish\n");
             exit(1);
           }
-          bufptr++;
+          parser->bufptr++;
         }
         
         // Constant
@@ -417,37 +416,37 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
         op->uv.ptr_value = const_info;
         SPerl_yylvalp->opval = op;
         
-        parser->bufptr = bufptr;
+        
         return CONST;
       }
       case '"': {
-        bufptr++;
+        parser->bufptr++;
         
         /* Save current position */
-        SPerl_char* cur_token_ptr = bufptr;
+        SPerl_char* cur_token_ptr = parser->bufptr;
         
         SPerl_char* str;
-        if (*(bufptr + 1) == '"') {
+        if (*(parser->bufptr + 1) == '"') {
           str = SPerl_PARSER_new_string(parser, 0);
           str[0] = '\0';
-          bufptr++;
-          bufptr++;
+          parser->bufptr++;
+          parser->bufptr++;
         }
         else {
-          while(*bufptr != '"' && *bufptr != '\0') {
-            bufptr++;
+          while(*parser->bufptr != '"' && *parser->bufptr != '\0') {
+            parser->bufptr++;
           }
-          if (*bufptr == '\0') {
+          if (*parser->bufptr == '\0') {
             fprintf(stderr, "syntax error: string don't finish\n");
             exit(1);
           }
           
-          size_t str_len = bufptr - cur_token_ptr;
+          size_t str_len = parser->bufptr - cur_token_ptr;
           str = SPerl_PARSER_new_string(parser, str_len);
           memcpy(str, cur_token_ptr, str_len);
           str[str_len] = '\0';
 
-          bufptr++;
+          parser->bufptr++;
         }
         
         SPerl_OP* op = SPerl_OP_newOP(parser, SPerl_OP_CONST, NULL, NULL);
@@ -457,7 +456,7 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
         const_info->uv.string_value = str;
         op->uv.ptr_value = const_info;
         
-        parser->bufptr = bufptr;
+        
         SPerl_yylvalp->opval = (SPerl_OP*)op;
         return CONST;
       }
@@ -465,16 +464,16 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
         /* Variable */
         if (c == '$') {
           /* Save current position */
-          SPerl_char* cur_token_ptr = bufptr;
+          SPerl_char* cur_token_ptr = parser->bufptr;
           
-          bufptr++;
+          parser->bufptr++;
 
           /* Next is graph */
-          while(isalnum(*bufptr) || (*bufptr) == '_' || (*bufptr) == ':') {
-            bufptr++;
+          while(isalnum(*parser->bufptr) || (*parser->bufptr) == '_' || (*parser->bufptr) == ':') {
+            parser->bufptr++;
           }
 
-          size_t str_len = bufptr - cur_token_ptr;
+          size_t str_len = parser->bufptr - cur_token_ptr;
           SPerl_char* var = SPerl_PARSER_new_string(parser, str_len);
           memcpy(var, cur_token_ptr, str_len);
           var[str_len] = '\0';
@@ -484,24 +483,24 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
           var_info->name = var;
           op->uv.ptr_value = var_info;
           
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->opval = (SPerl_OP*)op;
           return VAR;
         }
         /* Number literal */
         else if (isdigit(c)) {
-          SPerl_char* cur_token_ptr = bufptr;
+          SPerl_char* cur_token_ptr = parser->bufptr;
           
           SPerl_int point_count = 0;
           
-          bufptr++;
+          parser->bufptr++;
           
           // Scan number
-          while(isdigit(*bufptr) || *bufptr == '.') {
-            if (*bufptr == '.') {
+          while(isdigit(*parser->bufptr) || *parser->bufptr == '.') {
+            if (*parser->bufptr == '.') {
               point_count++;
             }
-            bufptr++;
+            parser->bufptr++;
           }
           
           if (point_count >= 2) {
@@ -510,7 +509,7 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
           }
           
           // Number literal
-          size_t str_len = bufptr - cur_token_ptr;
+          size_t str_len = parser->bufptr - cur_token_ptr;
           SPerl_char* num_str = malloc(str_len + 1);
           memcpy(num_str, cur_token_ptr, str_len);
           num_str[str_len] = '\0';
@@ -528,7 +527,7 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
             
             free(num_str);
             
-            parser->bufptr = bufptr;
+            
             SPerl_yylvalp->opval = (SPerl_OP*)op;
             
             return CONST;
@@ -545,7 +544,7 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
 
             free(num_str);
             
-            parser->bufptr = bufptr;
+            
             SPerl_yylvalp->opval = (SPerl_OP*)op;
             
             return CONST;
@@ -554,72 +553,72 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
         /* Keyword or word */
         else if (isalpha(c) || c == '_') {
           /* Save current position */
-          SPerl_char* cur_token_ptr = bufptr;
+          SPerl_char* cur_token_ptr = parser->bufptr;
           
-          bufptr++;
+          parser->bufptr++;
           
-          while(isalnum(*bufptr) || (*bufptr) == '_' || (*bufptr) == ':') {
-            bufptr++;
+          while(isalnum(*parser->bufptr) || (*parser->bufptr) == '_' || (*parser->bufptr) == ':') {
+            parser->bufptr++;
           }
           
-          size_t str_len = bufptr - cur_token_ptr;
+          size_t str_len = parser->bufptr - cur_token_ptr;
           SPerl_char* keyword = SPerl_PARSER_new_string(parser, str_len);
           memcpy(keyword, cur_token_ptr, str_len);
           keyword[str_len] = '\0';
           
           if (expect != SPerl_OP_EXPECT_WORD) {
             if (memcmp(keyword, "my", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return MY;
             }
             else if (memcmp(keyword, "has", str_len) == 0) {
               parser->expect = SPerl_OP_EXPECT_WORD;
-              parser->bufptr = bufptr;
+              
               return HAS;
             }
             else if (memcmp(keyword, "sub", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               parser->expect = SPerl_OP_EXPECT_WORD;
               return SUB;
             }
             else if (memcmp(keyword, "package", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return PACKAGE;
             }
             else if (memcmp(keyword, "if", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return IF;
             }
             else if (memcmp(keyword, "elsif", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return ELSIF;
             }
             else if (memcmp(keyword, "else", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return ELSE;
             }
             else if (memcmp(keyword, "return", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return RETURN;
             }
             else if (memcmp(keyword, "for", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return FOR;
             }
             else if (memcmp(keyword, "last", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return LAST;
             }
             else if (memcmp(keyword, "next", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return NEXT;
             }
             else if (memcmp(keyword, "use", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return USE;
             }
             else if (memcmp(keyword, "while", str_len) == 0) {
-              parser->bufptr = bufptr;
+              
               return WHILE;
             }
             else if (memcmp(keyword, "true", str_len) == 0) {
@@ -630,7 +629,7 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
               const_info->uv.int_value = 1;
               op->uv.ptr_value = const_info;
 
-              parser->bufptr = bufptr;
+              
               SPerl_yylvalp->opval = (SPerl_OP*)op;
 
               return CONST;
@@ -643,7 +642,7 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
               const_info->uv.int_value = 0;
               op->uv.ptr_value = const_info;
 
-              parser->bufptr = bufptr;
+              
               SPerl_yylvalp->opval = (SPerl_OP*)op;
 
               return CONST;
@@ -654,14 +653,14 @@ int SPerl_yylex(YYSTYPE* SPerl_yylvalp, SPerl_PARSER* parser) {
 
           op->uv.string_value = keyword;
           
-          parser->bufptr = bufptr;
+          
           SPerl_yylvalp->opval = (SPerl_OP*)op;
           return WORD;
         }
         
         /* Return character */
-        bufptr++;
-        parser->bufptr = bufptr;
+        parser->bufptr++;
+        
         return c;
     }
   }
