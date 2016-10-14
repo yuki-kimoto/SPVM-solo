ec0d9d857f78748c3ea56c2e932ced9f4eeb1f07
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-10-14 14:02:00 +0900

    cleanup

--- sperl_field_info.h
+++ sperl_field_info.h
@@ -7,9 +7,9 @@
 struct SPerl_field_info {
   SPerl_char* type;
   SPerl_char* name;
-  SPerl_CLASS_INFO* class_info;
-  SPerl_OP* op;
   SPerl_ARRAY* descripters;
+  SPerl_OP* op;
+  SPerl_CLASS_INFO* class_info;
 };
 
 SPerl_FIELD_INFO* SPerl_FIELD_INFO_new(SPerl_PARSER* parser);
