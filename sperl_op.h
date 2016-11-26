3fa2b9645b800ddc7e3dfbd0b464cb85e80268bc
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-11-26 17:52:42 +0900

    cleanup

--- sperl_op.h
+++ sperl_op.h
@@ -15,6 +15,9 @@ enum {                          // [GROUP]
   SPerl_OP_C_CODE_DECL_DESCRIPTER,   // UNKNOWN
   SPerl_OP_C_CODE_DECL_ANON_SUB,      // UNKNOWN
   SPerl_OP_C_CODE_DECL_ENUM_VALUE,    // UNKNOWN
+  SPerl_OP_C_CODE_ENUM_BLOCK,    // UNKNOWN
+  SPerl_OP_C_CODE_CLASS_BLOCK,   // UNKNOWN
+  SPerl_OP_C_CODE_TYPE,         // UNKNOWN
   SPerl_OP_C_CODE_CONSTANT,     // CONST
   SPerl_OP_C_CODE_BIT_NOT,       // UNOP
   SPerl_OP_C_CODE_PRE_INC,       // INTDEC
@@ -83,9 +86,6 @@ enum {                          // [GROUP]
   SPerl_OP_C_CODE_FOR,          // UNKNOWN
   SPerl_OP_C_CODE_WHILE,        // UNKNOWN
   SPerl_OP_C_CODE_DESCRIPTER_ENUM,         // UNKNOWN
-  SPerl_OP_C_CODE_ENUM_BLOCK,    // UNKNOWN
-  SPerl_OP_C_CODE_CLASS_BLOCK,   // UNKNOWN
-  SPerl_OP_C_CODE_TYPE,         // UNKNOWN
   SPerl_OP_C_CODE_GET_ENUM_VALUE, // UNKNOWN
   SPerl_OP_C_CODE_CONVERT_TYPE,  // UNKNOWN
   SPerl_OP_C_CODE_POP,          // UNKNOWN
