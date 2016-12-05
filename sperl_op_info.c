8ce53b4e2b6d4c2cf0916abb419608a17c0313ac
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-12-05 10:24:11 +0900

    cleanup bit_or

--- sperl_op_info.c
+++ sperl_op_info.c
@@ -33,8 +33,6 @@ SPerl_char* const SPerl_OP_INFO_C_CODE_NAMES[] = {
   "RETURN_DOUBLE",
   "RETURN_REF",
   "RETURN",
-  "BIT_OR_INT",
-  "BIT_OR_LONG",
   "BIT_XOR_INT",
   "BIT_XOR_LONG",
   "POP",
