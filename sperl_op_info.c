67c4d04ade90effa5b3b5de225ad8b047b116f26
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-12-05 15:48:36 +0900

    cleanup convert_type

--- sperl_op_info.c
+++ sperl_op_info.c
@@ -1,31 +1,6 @@
 #include "sperl_op_info.h"
 #include "sperl_parser.h"
 
-
-
-
-
-
-SPerl_char* const SPerl_OP_INFO_C_CODE_NAMES[] = {
-  "NULL",
-  "CONVERT_DOUBLE_TO_FLOAT",
-  "CONVERT_DOUBLE_TO_INT",
-  "CONVERT_DDOUBLE_TO_LONG",
-  "CONVERT_FLOAT_TO_DOUBLE",
-  "CONVERT_FLOAT_TO_INT",
-  "CONVERT_FLOAT_TO_LONG",
-  "CONVERT_INT_TO_BYTE",
-  "CONVERT_INT_TO_CHAR",
-  "CONVERT_INT_TO_DOUBLE",
-  "CONVERT_INT_TO_FLOAT",
-  "CONVERT_INT_TO_LONG",
-  "CONVERT_INT_TO_SHORT",
-  "CONVERT_LONG_TO_DOUBLE",
-  "CONVERT_LONG_TO_FLOAT",
-  "CONVERT_LONG_TO_INT",
-};
-
-
 SPerl_OP_INFO* SPerl_OP_INFO_new(SPerl_PARSER* parser) {
   return SPerl_PARSER_alloc_memory_pool(parser, sizeof(SPerl_OP_INFO));
 }
--- sperl_op_info.h
+++ sperl_op_info.h
@@ -3,17 +3,7 @@
 
 #include "sperl_base.h"
 
-
-
-
-enum {
-  SPerl_OP_INFO_C_CODE_NULL,
-};
-
-extern SPerl_char* const SPerl_OP_INFO_C_CODE_NAMES[];
-
 struct SPerl_op_info {
-  SPerl_int code;
   SPerl_RESOLVED_TYPE* return_resolved_type;
   SPerl_boolean lvalue;
 };
