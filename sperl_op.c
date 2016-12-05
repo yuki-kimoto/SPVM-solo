da611fca922333325109363de588005fe43da137
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-12-05 10:16:51 +0900

    cleanup remainder

--- sperl_op.c
+++ sperl_op.c
@@ -563,16 +563,16 @@ void SPerl_OP_create_vmcode(SPerl_PARSER* parser) {
               
               // Code
               SPerl_VMCODE* vmcode = SPerl_PARSER_new_vmcode(parser);
-              if (op_cur->uv.op_info->code == SPerl_OP_INFO_C_CODE_REMAINDER_INT) {
+              if (op_cur->uv.op_info->return_resolved_type->id <= SPerl_BODY_CORE_C_CODE_INT) {
                 vmcode->code = SPerl_VMCODE_C_CODE_REMAINDER_INT;
               }
-              else if (op_cur->uv.op_info->code == SPerl_OP_INFO_C_CODE_REMAINDER_LONG) {
+              else if (op_cur->uv.op_info->return_resolved_type->id == SPerl_BODY_CORE_C_CODE_LONG) {
                 vmcode->code = SPerl_VMCODE_C_CODE_REMAINDER_LONG;
               }
-              else if (op_cur->uv.op_info->code == SPerl_OP_INFO_C_CODE_REMAINDER_FLOAT) {
+              else if (op_cur->uv.op_info->return_resolved_type->id == SPerl_BODY_CORE_C_CODE_FLOAT) {
                 vmcode->code = SPerl_VMCODE_C_CODE_REMAINDER_FLOAT;
               }
-              else if (op_cur->uv.op_info->code == SPerl_OP_INFO_C_CODE_REMAINDER_DOUBLE) {
+              else if (op_cur->uv.op_info->return_resolved_type->id == SPerl_BODY_CORE_C_CODE_DOUBLE) {
                 vmcode->code = SPerl_VMCODE_C_CODE_REMAINDER_DOUBLE;
               }
               SPerl_VMCODES_push(vmcodes, vmcode);
@@ -1187,20 +1187,7 @@ void SPerl_OP_check_ops(SPerl_PARSER* parser) {
               SPerl_OP_insert_op_convert_type(parser, op_cur, first_resolved_type, last_resolved_type);
               
               SPerl_RESOLVED_TYPE* resolved_type = SPerl_OP_get_resolved_type(parser, op_cur->first);
-              SPerl_OP_INFO* op_info = op_cur->uv.op_info;
-              if (resolved_type->id <= SPerl_BODY_CORE_C_CODE_INT) {
-                op_info->code = SPerl_OP_INFO_C_CODE_REMAINDER_INT;
-              }
-              else if (resolved_type->id == SPerl_BODY_CORE_C_CODE_LONG) {
-                op_info->code = SPerl_OP_INFO_C_CODE_REMAINDER_LONG;
-              }
-              else if (resolved_type->id == SPerl_BODY_CORE_C_CODE_FLOAT) {
-                op_info->code = SPerl_OP_INFO_C_CODE_REMAINDER_FLOAT;
-              }
-              else if (resolved_type->id == SPerl_BODY_CORE_C_CODE_DOUBLE) {
-                op_info->code = SPerl_OP_INFO_C_CODE_REMAINDER_DOUBLE;
-              }
-              op_info->return_resolved_type = resolved_type;
+              op_cur->uv.op_info->return_resolved_type = resolved_type;
               
               break;
             }
