202705e13f669299a0190e67831e4463e14ad093
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-12-09 09:52:26 +0900

    cleanup parser

--- sperl_parser.c
+++ sperl_parser.c
@@ -1,35 +1,17 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <inttypes.h>
 
 #include "sperl_parser.h"
-#include "sperl_array.h"
-#include "sperl_hash.h"
-#include "sperl_constant.h"
-#include "sperl_field.h"
-#include "sperl_sub.h"
-#include "sperl_my_var.h"
-#include "sperl_var.h"
-#include "sperl_memory_pool.h"
-#include "sperl_op.h"
-#include "sperl_word.h"
-#include "sperl_enumeration_value.h"
-#include "sperl_descripter.h"
-#include "sperl_type.h"
-#include "sperl_type_component_word.h"
-#include "sperl_type_component_array.h"
-#include "sperl_type_component_sub.h"
-#include "sperl_body.h"
-#include "sperl_body_core.h"
-#include "sperl_body_class.h"
-#include "sperl_enumeration.h"
-#include "sperl_package.h"
-#include "sperl_op_info.h"
 #include "sperl_resolved_type.h"
-#include "sperl_constant_pool.h"
-#include "sperl_bytecode.h"
-#include "sperl_bytecodes.h"
+#include "sperl_package.h"
+#include "sperl_body_core.h"
+#include "sperl_body.h"
+#include "sperl_type_component_word.h"
+#include "sperl_type.h"
+#include "sperl_word.h"
+#include "sperl_op.h"
+#include "sperl_memory_pool.h"
+#include "sperl_hash.h"
+#include "sperl_array.h"
 
 SPerl_PARSER* SPerl_PARSER_new() {
   SPerl_PARSER* parser = calloc(1, sizeof(SPerl_PARSER));
