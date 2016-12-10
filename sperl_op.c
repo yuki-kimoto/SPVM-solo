f7bd44f20b1f51cb3e5a5defdf0c62add4ebd481
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-12-10 15:53:50 +0900

    add convert_and_to_if

--- sperl_op.c
+++ sperl_op.c
@@ -238,6 +238,73 @@ SPerl_RESOLVED_TYPE* SPerl_OP_get_resolved_type(SPerl_PARSER* parser, SPerl_OP* 
   return resolved_type;
 }
 
+SPerl_OP* SPerl_OP_newOP_CONSTANT_true(SPerl_PARSER* parser) {
+  SPerl_OP* op_constant_true = SPerl_OP_newOP(parser, SPerl_OP_C_CODE_CONSTANT, NULL, NULL);
+  SPerl_CONSTANT* constant_true = SPerl_CONSTANT_new(parser);
+  constant_true->code = SPerl_CONSTANT_C_CODE_INT;
+  constant_true->uv.int_value = 1;
+  op_constant_true->uv.constant = constant_true;
+  
+  return op_constant_true;
+}
+
+SPerl_OP* SPerl_OP_newOP_CONSTANT_false(SPerl_PARSER* parser) {
+  SPerl_OP* op_constant_false = SPerl_OP_newOP(parser, SPerl_OP_C_CODE_CONSTANT, NULL, NULL);
+  SPerl_CONSTANT* constant_false = SPerl_CONSTANT_new(parser);
+  constant_false->code = SPerl_CONSTANT_C_CODE_INT;
+  constant_false->uv.int_value = 0;
+  op_constant_false->uv.constant = constant_false;
+  
+  return op_constant_false;
+}
+
+void SPerl_OP_convert_and_to_if(SPerl_PARSER* parser, SPerl_OP* op) {
+  
+  /* before
+    AND
+      x
+      y
+  */
+  
+  /* after 
+    IF
+      x
+      IF
+        y
+        1
+        0
+      0
+  */
+  
+  SPerl_OP* op_first = op->first;
+  SPerl_OP* op_last = op->last;
+  
+  // Constant false 1
+  SPerl_OP* op_constant_false1 = SPerl_OP_newOP_CONSTANT_false(parser);
+  
+  // Constant false 2
+  SPerl_OP* op_constant_false2 = SPerl_OP_newOP_CONSTANT_false(parser);
+  
+  // Constant true
+  SPerl_OP* op_constant_true = SPerl_OP_newOP_CONSTANT_true(parser);
+  
+  // if
+  SPerl_OP* op_if = SPerl_OP_newOP(parser, SPerl_OP_C_CODE_IF, NULL, NULL);
+  
+  // and to if
+  op->code = SPerl_OP_C_CODE_IF;
+  
+  SPerl_OP_sibling_splice(parser, op_if, NULL, 0, op_last);
+  SPerl_OP_sibling_splice(parser, op_if, op_last, 0, op_constant_true);
+  SPerl_OP_sibling_splice(parser, op_if, op_constant_true, 0, op_constant_false1);
+  
+  op_first->sibparent = op_if;
+  
+  op_if->sibparent = op;
+  
+  SPerl_OP_sibling_splice(parser, op, op_if, 0, op_constant_false2);
+}
+
 void SPerl_OP_check_ops(SPerl_PARSER* parser) {
   for (SPerl_int i = 0; i < parser->op_subs->length; i++) {
     
@@ -297,6 +364,9 @@ void SPerl_OP_check_ops(SPerl_PARSER* parser) {
                 SPerl_yyerror_format(parser, "&& operator can use only condition context at %s line %d\n", op_cur->file, op_cur->line);
                 break;
               }
+              
+              // Convert && to if statement
+              SPerl_OP_convert_and_to_if(parser, op_cur);
               
               break;
             }
