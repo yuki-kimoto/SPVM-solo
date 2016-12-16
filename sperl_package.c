118593ad31f852650d6908fbd30d2470f782b1dc
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-12-16 14:45:07 +0900

    add core and body_class information to package

--- sperl_package.c
+++ sperl_package.c
@@ -1,7 +1,36 @@
 #include "sperl_package.h"
 #include "sperl_parser.h"
 #include "sperl_allocator.h"
+#include "sperl_constant_pool.h"
+
+SPerl_char* const SPerl_PACKAGE_C_CODE_NAMES[] = {
+  "boolean",
+  "char",
+  "byte",
+  "short",
+  "int",
+  "long",
+  "float",
+  "double"
+};
+
+SPerl_int const SPerl_PACKAGE_C_SIZES[] = {
+  1,
+  1,
+  1,
+  2,
+  4,
+  8,
+  4,
+  8
+};
 
 SPerl_PACKAGE* SPerl_PACKAGE_new(SPerl_PARSER* parser) {
-  return SPerl_ALLOCATOR_alloc_memory_pool(parser, sizeof(SPerl_PACKAGE));
+  SPerl_PACKAGE* package = SPerl_ALLOCATOR_alloc_memory_pool(parser, sizeof(SPerl_PACKAGE));
+  
+  package->op_constants = SPerl_ALLOCATOR_new_array(parser, 0);
+  package->constant_pool = SPerl_CONSTANT_POOL_new();
+  package->op_subs = SPerl_ALLOCATOR_new_array(parser, 0);
+  
+  return package;
 }
--- sperl_package.h
+++ sperl_package.h
@@ -3,9 +3,37 @@
 
 #include "sperl_base.h"
 
+enum {
+  SPerl_PACKAGE_C_CODE_LENGTH = 8
+};
+
+enum {
+  SPerl_PACKAGE_C_CODE_BOOLEAN,
+  SPerl_PACKAGE_C_CODE_CHAR,
+  SPerl_PACKAGE_C_CODE_BYTE,
+  SPerl_PACKAGE_C_CODE_SHORT,
+  SPerl_PACKAGE_C_CODE_INT,
+  SPerl_PACKAGE_C_CODE_LONG,
+  SPerl_PACKAGE_C_CODE_FLOAT,
+  SPerl_PACKAGE_C_CODE_DOUBLE
+};
+
+extern SPerl_char* const SPerl_PACKAGE_C_CODE_NAMES[];
+extern SPerl_int const SPerl_PACKAGE_C_SIZES[];
+
 struct SPerl_package {
   SPerl_OP* op_name;
   SPerl_OP* op_type;
+  SPerl_int code;
+  SPerl_int size;
+
+  SPerl_ARRAY* op_descripters;
+  SPerl_ARRAY* op_fields;
+  SPerl_HASH* field_symtable;
+  SPerl_boolean is_value;
+  SPerl_ARRAY* op_subs;
+  SPerl_ARRAY* op_constants;
+  SPerl_CONSTANT_POOL* constant_pool;
 };
 
 SPerl_PACKAGE* SPerl_PACKAGE_new(SPerl_PARSER* parser);
