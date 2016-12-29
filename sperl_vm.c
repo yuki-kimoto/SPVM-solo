fbd4fd320b2a556e6ada9b73994892a86dedd024
Yuki Kimoto <kimoto.yuki@gmail.com>
2016-12-29 11:51:57 +0900

    get bytecodes

--- sperl_vm.c
+++ sperl_vm.c
@@ -1,6 +1,12 @@
+#include <string.h>
+
 #include "sperl_vm.h"
 #include "sperl_parser.h"
 #include "sperl_allocator.h"
+#include "sperl_hash.h"
+#include "sperl_bytecodes.h"
+#include "sperl_bytecode.h"
+#include "sperl_sub.h"
 
 SPerl_VM* SPerl_VM_new(SPerl_PARSER* parser) {
   return SPerl_ALLOCATOR_alloc_memory_pool(parser, sizeof(SPerl_VM));
@@ -10,5 +16,8 @@ void SPerl_VM_run(SPerl_PARSER* parser) {
   uint8_t* entry_point = parser->entry_point;
   
   SPerl_SUB* sub_entry_point = SPerl_HASH_search(parser->sub_abs_name_symtable, entry_point, strlen(entry_point));
+  
+  SPerl_BYTECODES* bytecodes = sub_entry_point->bytecodes;
+  
   
 }
