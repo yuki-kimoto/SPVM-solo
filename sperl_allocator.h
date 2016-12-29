7e2149f4a3026730ebe2fbbd3f7915f17d631fa6
Koichi Murase <myoga.murase@gmail.com>
2016-12-30 00:46:20 +0900

    fix-uint8_t: sperl_allocator

--- sperl_allocator.h
+++ sperl_allocator.h
@@ -5,7 +5,7 @@
 
 SPerl_ARRAY* SPerl_ALLOCATOR_new_array(SPerl_PARSER* parser, int32_t capacity);
 SPerl_HASH* SPerl_ALLOCATOR_new_hash(SPerl_PARSER* parser, int32_t capacity);
-uint8_t* SPerl_ALLOCATOR_new_string(SPerl_PARSER* parser, int32_t length);
+char* SPerl_ALLOCATOR_new_string(SPerl_PARSER* parser, int32_t length);
 int32_t* SPerl_ALLOCATOR_new_int(SPerl_PARSER* parser);
 SPerl_VMCODE* SPerl_ALLOCATOR_new_vmcode(SPerl_PARSER* parser);
 
