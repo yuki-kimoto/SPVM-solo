3c445fde2e43dfe75f8a190ca4529c3b74077631
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-01-11 09:22:04 +0900

    add CALLSUB bytecode

--- sperl_vm.c
+++ sperl_vm.c
@@ -833,6 +833,9 @@ void SPerl_VM_run(SPerl* sperl, const char* sub_name) {
       case SPerl_BYTECODE_C_CODE_BREAKPOINT:
       
         break;
+      case SPerl_BYTECODE_C_CODE_CALLSUB:
+      
+        break;
       case SPerl_BYTECODE_C_CODE_UNDEFINED:
       
         break;
