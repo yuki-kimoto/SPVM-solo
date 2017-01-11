f0da984512d9a2a5c7cd461fd536a10007df0790
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-01-11 21:18:11 +0900

    improve constant_pool extend

--- sperl_constant_pool.c
+++ sperl_constant_pool.c
@@ -15,11 +15,11 @@ SPerl_CONSTANT_POOL* SPerl_CONSTANT_POOL_new() {
   return constant_pool;
 }
 
-void SPerl_CONSTANT_POOL_maybe_extend(SPerl_CONSTANT_POOL* constant_pool) {
+void SPerl_CONSTANT_POOL_extend(SPerl_CONSTANT_POOL* constant_pool, int32_t extend) {
   int32_t length = constant_pool->length;
   int32_t capacity = constant_pool->capacity;
   
-  if (length + 2 >= capacity) {
+  if (length + extend >= capacity) {
     int32_t new_capacity = capacity * 2;
     constant_pool->values = realloc(constant_pool->values, new_capacity * sizeof(int32_t));
     memset(constant_pool->values + capacity, 0, (new_capacity - capacity) * sizeof(int32_t));
@@ -30,7 +30,7 @@ void SPerl_CONSTANT_POOL_maybe_extend(SPerl_CONSTANT_POOL* constant_pool) {
 void SPerl_CONSTANT_POOL_push_int(SPerl_CONSTANT_POOL* constant_pool, int32_t value) {
   int32_t length = constant_pool->length;
   
-  SPerl_CONSTANT_POOL_maybe_extend(constant_pool);
+  SPerl_CONSTANT_POOL_extend(constant_pool, 4);
   
   constant_pool->values[length] = value;
   constant_pool->length++;
@@ -39,7 +39,7 @@ void SPerl_CONSTANT_POOL_push_int(SPerl_CONSTANT_POOL* constant_pool, int32_t va
 void SPerl_CONSTANT_POOL_push_long(SPerl_CONSTANT_POOL* constant_pool, int64_t value) {
   int32_t length = constant_pool->length;
   
-  SPerl_CONSTANT_POOL_maybe_extend(constant_pool);
+  SPerl_CONSTANT_POOL_extend(constant_pool, 8);
   
   *(int64_t*)&constant_pool->values[length] = value;
   constant_pool->length += 2;
@@ -48,7 +48,7 @@ void SPerl_CONSTANT_POOL_push_long(SPerl_CONSTANT_POOL* constant_pool, int64_t v
 void SPerl_CONSTANT_POOL_push_float(SPerl_CONSTANT_POOL* constant_pool, float value) {
   int32_t length = constant_pool->length;
   
-  SPerl_CONSTANT_POOL_maybe_extend(constant_pool);
+  SPerl_CONSTANT_POOL_extend(constant_pool, 4);
   
   *(float*)&constant_pool->values[length] = value;
   constant_pool->length++;
@@ -57,7 +57,7 @@ void SPerl_CONSTANT_POOL_push_float(SPerl_CONSTANT_POOL* constant_pool, float va
 void SPerl_CONSTANT_POOL_push_double(SPerl_CONSTANT_POOL* constant_pool, double value) {
   int32_t length = constant_pool->length;
   
-  SPerl_CONSTANT_POOL_maybe_extend(constant_pool);
+  SPerl_CONSTANT_POOL_extend(constant_pool, 8);
   
   *(double*)&constant_pool->values[length] = value;
   constant_pool->length += 2;
