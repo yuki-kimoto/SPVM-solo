fb3f9bbc2bbe1973fcfd13b8aaf5ddd9fc0293d0
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-01-23 22:12:11 +0900

    fix dump constant

--- sperl_dumper.c
+++ sperl_dumper.c
@@ -182,7 +182,7 @@ void SPerl_DUMPER_dump_resolved_types(SPerl* sperl, SPerl_ARRAY* resolved_types)
 void SPerl_DUMPER_dump_constant_pool(SPerl* sperl, SPerl_CONSTANT_POOL* constant_pool) {
   (void)sperl;
 
-  for (int32_t i = 0; i <= constant_pool->length; i++) {
+  for (int32_t i = 0; i < constant_pool->length; i++) {
     printf("      constant_pool[%" PRId32 "] %" PRId32 "\n", i, constant_pool[i]);
   }
 }
