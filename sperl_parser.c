fa9c716c3dea8383e0cfb87740fb47cb5a777edd
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-01-30 23:37:19 +0900

    core type is not package

--- sperl_parser.c
+++ sperl_parser.c
@@ -71,21 +71,6 @@ SPerl_PARSER* SPerl_PARSER_new(SPerl* sperl) {
     
     // Add type
     SPerl_ARRAY_push(parser->op_types, op_type);
-    
-    // Package
-    SPerl_PACKAGE* package = SPerl_PACKAGE_new(sperl);
-    package->byte_size = SPerl_RESOLVED_TYPE_C_CORE_SIZES[i];
-    SPerl_OP* op_package_name = SPerl_OP_newOP(sperl, SPerl_OP_C_CODE_NAME, "CORE", 1);
-    op_package_name->uv.name = name;
-    package->op_name = op_package_name;
-    package->op_type = op_type;
-    
-    // Package OP
-    SPerl_OP* op_package = SPerl_OP_newOP(sperl, SPerl_OP_C_CODE_DECL_PACKAGE, "CORE", 1);
-    op_package->uv.package = package;
-    
-    SPerl_ARRAY_push(parser->op_packages, op_package);
-    SPerl_HASH_insert(parser->package_symtable, name, strlen(name), type);
   }
   
   // Core array types
