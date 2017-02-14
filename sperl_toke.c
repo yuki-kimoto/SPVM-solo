44411e721c30c48192b94941c6b9d9efecd4a45e
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-02-14 16:34:57 +0900

    tokenize byte, short constant

--- sperl_toke.c
+++ sperl_toke.c
@@ -520,6 +520,18 @@ int SPerl_yylex(SPerl_YYSTYPE* yylvalp, SPerl* sperl) {
             constant_code = SPerl_CONSTANT_C_CODE_DOUBLE;
             parser->bufptr++;
           }
+          else if (*parser->bufptr == 'b' || *parser->bufptr == 'B') {
+            constant_code = SPerl_CONSTANT_C_CODE_BYTE;
+            parser->bufptr++;
+          }
+          else if (*parser->bufptr == 's' || *parser->bufptr == 'S') {
+            constant_code = SPerl_CONSTANT_C_CODE_SHORT;
+            parser->bufptr++;
+          }
+          else if (*parser->bufptr == 'i' || *parser->bufptr == 'I') {
+            constant_code = SPerl_CONSTANT_C_CODE_INT;
+            parser->bufptr++;
+          }
           else if (*parser->bufptr == 'l' || *parser->bufptr == 'L') {
             constant_code = SPerl_CONSTANT_C_CODE_LONG;
             parser->bufptr++;
