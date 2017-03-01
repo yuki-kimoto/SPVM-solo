a9681edea2f2fe4a628ef7b6435f3487b798a858
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-03-01 09:21:21 +0900

    Revert "add VIRTUAL_MEMORY_alloc"
    This reverts commit 676746de22d9d24b33a6f64da65ff0b59e18ef65.

--- sperl_virtual_memory.c
+++ sperl_virtual_memory.c
@@ -6,19 +6,5 @@
 SPerl_VIRTUAL_MEMORY* SPerl_VIRTUAL_MEMORY_new(SPerl* sperl) {
   SPerl_VIRTUAL_MEMORY* virtual_memory = SPerl_ALLOCATOR_alloc_memory_pool(sperl, sizeof(SPerl_VIRTUAL_MEMORY));
   
-  virtual_memory->current_index = 0;
-  virtual_memory->indexes = malloc(1024 * sizeof(int64_t));
-  
   return virtual_memory;
-}
-
-SPerl_VIRTUAL_MEMORY* SPerl_VIRTUAL_MEMORY_malloc(SPerl* sperl, SPerl_VIRTUAL_MEMORY* virtual_memory, size_t byte_size) {
-  
-  int64_t current_index = virtual_memory->current_index;
-  
-  virtual_memory->indexes[current_index] = (intptr_t)malloc(byte_size);
-  
-  virtual_memory->current_index++;
-  
-  return current_index;
 }
--- sperl_virtual_memory.h
+++ sperl_virtual_memory.h
@@ -5,11 +5,9 @@
 
 // Method information
 struct SPerl_virtual_memory {
-  int64_t current_index;
   intptr_t* indexes;
 };
 
 SPerl_VIRTUAL_MEMORY* SPerl_VIRTUAL_MEMORY_new(SPerl* sperl);
-SPerl_VIRTUAL_MEMORY* SPerl_VIRTUAL_MEMORY_malloc(SPerl* sperl, SPerl_VIRTUAL_MEMORY* virtual_memory, size_t byte_size);
 
 #endif
