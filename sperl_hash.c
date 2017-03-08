df507898c0c9c81361d3d2e2d53de69304472507
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-03-08 10:23:11 +0900

    use allocator util in allocator_util.c

--- sperl_hash.c
+++ sperl_hash.c
@@ -4,7 +4,7 @@
 #include "sperl_hash.h"
 #include "sperl_hash_entry.h"
 #include "sperl_hash_func.h"
-#include "sperl_allocator_parser.h"
+#include "sperl_allocator_util.h"
 #include "sperl_parser.h"
 #include "sperl.h"
 
@@ -15,13 +15,13 @@ SPerl_HASH* SPerl_HASH_new(SPerl* sperl, int32_t capacity) {
     capacity = 101;
   }
   
-  SPerl_HASH* hash = SPerl_ALLOCATOR_PARSER_safe_malloc(sperl, sperl->parser, capacity, sizeof(SPerl_HASH));
+  SPerl_HASH* hash = SPerl_ALLOCATOR_UTIL_safe_malloc(capacity, sizeof(SPerl_HASH));
   memset(hash, 0, sizeof(SPerl_HASH));
   
   hash->count = 0;
   hash->capacity = capacity;
   
-  SPerl_HASH_ENTRY** entries = SPerl_ALLOCATOR_PARSER_safe_malloc_zero(sperl, sperl->parser, hash->capacity, sizeof(SPerl_HASH_ENTRY*));
+  SPerl_HASH_ENTRY** entries = SPerl_ALLOCATOR_UTIL_safe_malloc_zero(hash->capacity, sizeof(SPerl_HASH_ENTRY*));
   hash->entries = entries;
   
   return hash;
