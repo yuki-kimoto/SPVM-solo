179dbccf0e6509e8288e1390e1e9aac011c46819
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-03-08 10:36:16 +0900

    remove some functions from allocator parser

--- sperl_allocator_parser.c
+++ sperl_allocator_parser.c
@@ -5,70 +5,9 @@
 #include "sperl_array.h"
 #include "sperl_hash.h"
 #include "sperl_memory_pool.h"
+#include "sperl_allocator_util.h"
 #include "sperl_allocator_parser.h"
 #include "sperl_parser.h"
-
-void SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(SPerl* sperl, SPerl_PARSER* parser) {
-  (void)sperl;
-  fprintf(stderr, "Failed to allocate memory. Sperl will exit.\n");
-  exit(EXIT_FAILURE);
-  /*NOTREACHED*/
-}
-
-void* SPerl_ALLOCATOR_PARSER_safe_malloc(SPerl* sperl, SPerl_PARSER* parser, size_t count, size_t size) {
-  if (count > SIZE_MAX / size) {
-    SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, parser);
-  }
-
-  size_t const allocation_size = count * size;
-  if (allocation_size == 0) {
-    SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, parser);
-  }
-
-  void* const block = malloc(allocation_size);
-  if (!block) {
-    SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, parser);
-  }
-
-  return block;
-}
-
-void* SPerl_ALLOCATOR_PARSER_safe_malloc_zero(SPerl* sperl, SPerl_PARSER* parser, size_t count, size_t size) {
-  if (count > SIZE_MAX / size) {
-    SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, parser);
-  }
-
-  size_t const allocation_size = count * size;
-  if (allocation_size == 0) {
-    SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, parser);
-  }
-
-  void* const block = calloc(count, size);
-  if (!block) {
-    SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, parser);
-  }
-
-  return block;
-}
-
-void* SPerl_ALLOCATOR_PARSER_safe_realloc(SPerl* sperl, SPerl_PARSER* parser, void* ptr, size_t count, size_t size) {
-  if (count > SIZE_MAX / size) {
-    SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, parser);
-  }
-
-  size_t const allocation_size = count * size;
-  if (allocation_size == 0) {
-    SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, parser);
-  }
-
-  void* const block = realloc(ptr, allocation_size);
-  if (!block) {
-    free(ptr);
-    SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, parser);
-  }
-
-  return block;
-}
 
 void* SPerl_ALLOCATOR_PARSER_alloc_memory_pool(SPerl* sperl, SPerl_PARSER* parser, size_t size) {
   return SPerl_MEMORY_POOL_alloc(sperl, parser->memory_pool, size);
@@ -103,7 +42,7 @@ char* SPerl_ALLOCATOR_PARSER_new_string(SPerl* sperl, SPerl_PARSER* parser, size
     str = (char*) SPerl_MEMORY_POOL_alloc(sperl, parser->memory_pool, length + 1);
   }
   else {
-    str = (char*) SPerl_ALLOCATOR_PARSER_safe_malloc(sperl, parser, length + 1, sizeof(char));
+    str = (char*) SPerl_ALLOCATOR_UTIL_safe_malloc(length + 1, sizeof(char));
     SPerl_ARRAY_push(sperl, parser->long_strings, str);
   }
   
--- sperl_allocator_parser.h
+++ sperl_allocator_parser.h
@@ -3,11 +3,6 @@
 #include <stddef.h>
 #include "sperl_base.h"
 
-void SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(SPerl* sperl, SPerl_PARSER* parser);
-void* SPerl_ALLOCATOR_PARSER_safe_malloc(SPerl* sperl, SPerl_PARSER* parser, size_t count, size_t size);
-void* SPerl_ALLOCATOR_PARSER_safe_malloc_zero(SPerl* sperl, SPerl_PARSER* parser, size_t count, size_t size);
-void* SPerl_ALLOCATOR_PARSER_safe_realloc(SPerl* sperl, SPerl_PARSER* parser, void* ptr, size_t count, size_t size);
-
 SPerl_ARRAY* SPerl_ALLOCATOR_PARSER_new_array(SPerl* sperl, SPerl_PARSER* parser, size_t capacity);
 SPerl_HASH* SPerl_ALLOCATOR_PARSER_new_hash(SPerl* sperl, SPerl_PARSER* parser, size_t capacity);
 char* SPerl_ALLOCATOR_PARSER_new_string(SPerl* sperl, SPerl_PARSER* parser, size_t length);
