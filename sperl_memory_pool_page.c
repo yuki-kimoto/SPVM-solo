52472af6e4497f8160ff14792081f2c3d3419f28
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-03-08 10:30:03 +0900

    use allocator util in memory_pool_page.c

--- sperl_memory_pool_page.c
+++ sperl_memory_pool_page.c
@@ -2,10 +2,10 @@
 
 #include "sperl.h"
 #include "sperl_memory_pool_page.h"
-#include "sperl_allocator_parser.h"
+#include "sperl_allocator_util.h"
 
 SPerl_MEMORY_POOL_PAGE* SPerl_MEMORY_POOL_PAGE_new(SPerl* sperl) {
-  return (SPerl_MEMORY_POOL_PAGE*) SPerl_ALLOCATOR_PARSER_safe_malloc_zero(sperl, sperl->parser, 1, sizeof(SPerl_MEMORY_POOL_PAGE));
+  return (SPerl_MEMORY_POOL_PAGE*) SPerl_ALLOCATOR_UTIL_safe_malloc_zero(1, sizeof(SPerl_MEMORY_POOL_PAGE));
 }
 
 void SPerl_MEMORY_POOL_PAGE_free(SPerl* sperl, SPerl_MEMORY_POOL_PAGE* page) {
