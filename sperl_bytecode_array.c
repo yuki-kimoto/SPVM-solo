fcddbf3c7df8cd3c8341600af4b00c7a55df2ab5
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-03-08 10:10:00 +0900

    use allocator util in bytecode_array.c

--- sperl_bytecode_array.c
+++ sperl_bytecode_array.c
@@ -3,31 +3,29 @@
 
 #include "sperl.h"
 #include "sperl_bytecode_array.h"
-#include "sperl_allocator_parser.h"
+#include "sperl_allocator_util.h"
 #include "sperl_parser.h"
 
 SPerl_BYTECODE_ARRAY* SPerl_BYTECODE_ARRAY_new(SPerl* sperl) {
-  SPerl_PARSER* parser = sperl->parser;
   
-  SPerl_BYTECODE_ARRAY* bytecodes = SPerl_ALLOCATOR_PARSER_safe_malloc(sperl, parser, 1, sizeof(SPerl_BYTECODE_ARRAY));
+  SPerl_BYTECODE_ARRAY* bytecodes = SPerl_ALLOCATOR_UTIL_safe_malloc(1, sizeof(SPerl_BYTECODE_ARRAY));
   bytecodes->capacity = 64;
   bytecodes->length = 0;
   
-  uint8_t* values = SPerl_ALLOCATOR_PARSER_safe_malloc_zero(sperl, parser, bytecodes->capacity, sizeof(uint8_t));
+  uint8_t* values = SPerl_ALLOCATOR_UTIL_safe_malloc_zero(bytecodes->capacity, sizeof(uint8_t));
   bytecodes->values = values;
   
   return bytecodes;
 }
 
 void SPerl_BYTECODE_ARRAY_push(SPerl* sperl, SPerl_BYTECODE_ARRAY* bytecodes, uint8_t value) {
-  SPerl_PARSER* parser = sperl->parser;
   
   int32_t length = bytecodes->length;
   int32_t capacity = bytecodes->capacity;
   
   if (length >= capacity) {
     int32_t new_capacity = capacity * 2;
-    bytecodes->values = (uint8_t*) SPerl_ALLOCATOR_PARSER_safe_realloc(sperl, parser, bytecodes->values, new_capacity, sizeof(uint8_t));
+    bytecodes->values = (uint8_t*) SPerl_ALLOCATOR_UTIL_safe_realloc(bytecodes->values, new_capacity, sizeof(uint8_t));
     memset(bytecodes->values + capacity, 0, (new_capacity - capacity) * sizeof(uint8_t));
     bytecodes->capacity = new_capacity;
   }
