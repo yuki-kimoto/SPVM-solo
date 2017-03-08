866768befa5064374d32ba13378add3dee3bef87
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-03-08 10:27:02 +0900

    use allocator util in heap.c

--- sperl_heap.c
+++ sperl_heap.c
@@ -2,12 +2,12 @@
 
 #include "sperl.h"
 #include "sperl_heap.h"
-#include "sperl_allocator_parser.h"
+#include "sperl_allocator_util.h"
 
 void* SPerl_HEAP_alloc(SPerl* sperl, int32_t size) {
   (void) sperl;
   if (size < 0 || SIZE_MAX < (uint32_t) size) {
-    SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, sperl->parser);
+    SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
   }
-  return SPerl_ALLOCATOR_PARSER_safe_malloc(sperl, sperl->parser, size, sizeof(uint8_t));
+  return SPerl_ALLOCATOR_UTIL_safe_malloc(size, sizeof(uint8_t));
 }
