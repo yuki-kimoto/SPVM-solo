8c4b61d0342c5d324949a04ad55cd70d0531dcb9
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-03-08 10:08:17 +0900

    use allocator util in array.c

--- sperl_array.c
+++ sperl_array.c
@@ -3,13 +3,12 @@
 
 #include "sperl.h"
 #include "sperl_array.h"
-#include "sperl_allocator_parser.h"
+#include "sperl_allocator_util.h"
 #include "sperl_parser.h"
 
 SPerl_ARRAY* SPerl_ARRAY_new(SPerl* sperl, size_t capacity) {
-  SPerl_PARSER* parser = sperl->parser;
   
-  SPerl_ARRAY* array = SPerl_ALLOCATOR_PARSER_safe_malloc(sperl, parser, 1, sizeof(SPerl_ARRAY));
+  SPerl_ARRAY* array = SPerl_ALLOCATOR_UTIL_safe_malloc(1, sizeof(SPerl_ARRAY));
   array->length = 0;
   
   if (capacity == 0) {
@@ -19,24 +18,23 @@ SPerl_ARRAY* SPerl_ARRAY_new(SPerl* sperl, size_t capacity) {
     array->capacity = capacity;
   }
   
-  void** values = SPerl_ALLOCATOR_PARSER_safe_malloc_zero(sperl, parser, array->capacity, sizeof(void*));
+  void** values = SPerl_ALLOCATOR_UTIL_safe_malloc_zero(array->capacity, sizeof(void*));
   array->values = values;
   
   return array;
 }
 
 void SPerl_ARRAY_push(SPerl* sperl, SPerl_ARRAY* array, const void* value) {
-  SPerl_PARSER* parser = sperl->parser;
-  
+
   size_t length = array->length;
   size_t capacity = array->capacity;
   
   if (length >= capacity) {
     if (capacity > SIZE_MAX / 2) {
-      SPerl_ALLOCATOR_PARSER_exit_with_malloc_failure(sperl, parser);
+      SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
     }
     size_t new_capacity = capacity * 2;
-    array->values = (void**) SPerl_ALLOCATOR_PARSER_safe_realloc(sperl, parser, array->values, new_capacity, sizeof(void*));
+    array->values = (void**) SPerl_ALLOCATOR_UTIL_safe_realloc(array->values, new_capacity, sizeof(void*));
     memset(array->values + capacity, 0, (new_capacity - capacity) * sizeof(void*));
     array->capacity = new_capacity;
   }
