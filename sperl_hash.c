7be6a1c74cbf22125d7d34d16293558b07f299f8
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-03-22 10:41:20 +0900

    add perl one liner to implment template

--- sperl_hash.c
+++ sperl_hash.c
@@ -70,9 +70,13 @@ void SPerl_HASH_free(SPerl* sperl, SPerl_HASH* hash) {
 This is function templates shared with hash manipulate functions.
 All functions is genereted from the following templates.
 
-%SUFFIX% : function name suffix
-%TYPE% : type name
+You can create implementations by the following Perl one liner easily.
+  
+  perl -E 'my $suffix = "address"; my $type = "void*"; my $start = 0; while (my $line = <>) { if ($line =~ /^\[END_TEMPLATE\]/) { last; } if ($start) { $line =~ s/%SUFFIX%/$suffix/g; $line =~ s/%TYPE%/$type/g; print $line; }  if ($line =~ /^\[START_TEMPLATE\]/) { $start = 1 }  }' sperl_hash.c
+  
+  perl -E 'my $suffix = "long"; my $type = "int64_t"; my $start = 0; while (my $line = <>) { if ($line =~ /^\[END_TEMPLATE\]/) { last; } if ($start) { $line =~ s/%SUFFIX%/$suffix/g; $line =~ s/%TYPE%/$type/g; print $line; }  if ($line =~ /^\[START_TEMPLATE\]/) { $start = 1 }  }' sperl_hash.c
 
+[START_TEMPLATE]
 // Implementation
 // SUFFIX : %SUFFIX%
 // TYPE : %TYPE%
@@ -97,7 +101,7 @@ int64_t SPerl_HASH_new_hash_entry_%SUFFIX%(SPerl* sperl, SPerl_HASH* hash, const
   return index;
 }
 
-void SPerl_HASH_rehash(SPerl* sperl, SPerl_HASH* hash, int64_t new_table_capacity) {
+void SPerl_HASH_rehash_%SUFFIX%(SPerl* sperl, SPerl_HASH* hash, int64_t new_table_capacity) {
   (void)sperl;
   
   assert(hash);
@@ -178,7 +182,7 @@ void SPerl_HASH_insert_%SUFFIX%(SPerl* sperl, SPerl_HASH* hash, const char* key,
   if (hash->entries_length > hash->table_capacity * 0.75) {
     int64_t new_table_capacity = (hash->table_capacity * 2) + 1;
     
-    SPerl_HASH_rehash(sperl, hash, new_table_capacity);
+    SPerl_HASH_rehash_%SUFFIX%(sperl, hash, new_table_capacity);
   }
   
   SPerl_HASH_insert_norehash_%SUFFIX%(sperl, hash, key, length, value);
@@ -214,6 +218,7 @@ void SPerl_HASH_insert_%SUFFIX%(SPerl* sperl, SPerl_HASH* hash, const char* key,
     }
   }
 }
+[END_TEMPLATE]
 */
 
 // Implementation
@@ -221,50 +226,50 @@ void SPerl_HASH_insert_%SUFFIX%(SPerl* sperl, SPerl_HASH* hash, const char* key,
 // TYPE : void*
 int64_t SPerl_HASH_new_hash_entry_address(SPerl* sperl, SPerl_HASH* hash, const char* key, void* value) {
   (void)sperl;
-  
+
   assert(hash);
   assert(key);
-  
+
   int64_t index = hash->entries_length;
-  
+
   SPerl_HASH_maybe_extend_entries(sperl, hash);
-  
+
   SPerl_HASH_ENTRY* hash_entry = &hash->entries[index];
-  
+
   hash_entry->key = key;
   *(void**)&hash_entry->value = value;
   hash_entry->next_index = -1;
-  
+
   hash->entries_length++;
-  
+
   return index;
 }
 
-void SPerl_HASH_rehash(SPerl* sperl, SPerl_HASH* hash, int64_t new_table_capacity) {
+void SPerl_HASH_rehash_address(SPerl* sperl, SPerl_HASH* hash, int64_t new_table_capacity) {
   (void)sperl;
-  
+
   assert(hash);
   assert(new_table_capacity > 0);
-  
+
   SPerl_HASH_ENTRY** table = hash->table;
-  
+
   // Create new hash
   SPerl_HASH* new_hash = SPerl_HASH_new(sperl, new_table_capacity);
-  
+
   // Rehash
   int64_t i;
   for (i = 0; i < hash->entries_length; i++) {
     SPerl_HASH_ENTRY* entry = &hash->entries[i];
-    
+
     const char* key = entry->key;
-    
+
     assert(key);
-    
+
     const void* value = *(void**)&entry->value;
-    
+
     SPerl_HASH_insert_norehash_address(sperl, new_hash, key, strlen(key), value);
   }
-  
+
   // Replace hash fields
   free(hash->table);
   free(hash->entries);
@@ -277,14 +282,14 @@ void SPerl_HASH_rehash(SPerl* sperl, SPerl_HASH* hash, int64_t new_table_capacit
 
 void SPerl_HASH_insert_norehash_address(SPerl* sperl, SPerl_HASH* hash, const char* key, int64_t length, void* value) {
   (void)sperl;
-  
+
   assert(hash);
   assert(key);
   assert(length > 0);
-  
+
   int64_t hash_value = SPerl_HASH_FUNC_calc_hash(sperl, key, length);
   int64_t index = hash_value % hash->table_capacity;
-  
+
   if (hash->table[index]) {
     SPerl_HASH_ENTRY* next_entry = hash->table[index];
     while (1) {
@@ -312,36 +317,178 @@ void SPerl_HASH_insert_norehash_address(SPerl* sperl, SPerl_HASH* hash, const ch
 
 void SPerl_HASH_insert_address(SPerl* sperl, SPerl_HASH* hash, const char* key, int64_t length, void* value) {
   (void)sperl;
-  
+
   assert(hash);
   assert(key);
   assert(length > 0);
-  
+
   // Rehash
   if (hash->entries_length > hash->table_capacity * 0.75) {
     int64_t new_table_capacity = (hash->table_capacity * 2) + 1;
-    
-    SPerl_HASH_rehash(sperl, hash, new_table_capacity);
+
+    SPerl_HASH_rehash_address(sperl, hash, new_table_capacity);
   }
-  
+
   SPerl_HASH_insert_norehash_address(sperl, hash, key, length, value);
 }
 
 void* SPerl_HASH_search_address(SPerl* sperl, SPerl_HASH* hash, const char* key, int64_t length) {
   (void)sperl;
-  
+
   assert(hash);
   assert(key);
   assert(length > 0);
 
   int64_t hash_value = SPerl_HASH_FUNC_calc_hash(sperl, key, length);
   int64_t index = hash_value % hash->table_capacity;
-  
+
   SPerl_HASH_ENTRY* next_entry = hash->table[index];
   while (1) {
     if (next_entry) {
       if (strncmp(key, next_entry->key, length) == 0) {
         return *(void**)&next_entry->value;
+      }
+      else {
+        if (next_entry->next_index == -1) {
+          next_entry = NULL;
+        }
+        else {
+          next_entry = &hash->entries[next_entry->next_index];
+        }
+      }
+    }
+    else {
+      return NULL;
+    }
+  }
+}
+
+// Implementation
+// SUFFIX : long
+// TYPE : int64_t
+int64_t SPerl_HASH_new_hash_entry_long(SPerl* sperl, SPerl_HASH* hash, const char* key, int64_t value) {
+  (void)sperl;
+
+  assert(hash);
+  assert(key);
+
+  int64_t index = hash->entries_length;
+
+  SPerl_HASH_maybe_extend_entries(sperl, hash);
+
+  SPerl_HASH_ENTRY* hash_entry = &hash->entries[index];
+
+  hash_entry->key = key;
+  *(int64_t*)&hash_entry->value = value;
+  hash_entry->next_index = -1;
+
+  hash->entries_length++;
+
+  return index;
+}
+
+void SPerl_HASH_rehash_long(SPerl* sperl, SPerl_HASH* hash, int64_t new_table_capacity) {
+  (void)sperl;
+
+  assert(hash);
+  assert(new_table_capacity > 0);
+
+  SPerl_HASH_ENTRY** table = hash->table;
+
+  // Create new hash
+  SPerl_HASH* new_hash = SPerl_HASH_new(sperl, new_table_capacity);
+
+  // Rehash
+  int64_t i;
+  for (i = 0; i < hash->entries_length; i++) {
+    SPerl_HASH_ENTRY* entry = &hash->entries[i];
+
+    const char* key = entry->key;
+
+    assert(key);
+
+    const int64_t value = *(int64_t*)&entry->value;
+
+    SPerl_HASH_insert_norehash_long(sperl, new_hash, key, strlen(key), value);
+  }
+
+  // Replace hash fields
+  free(hash->table);
+  free(hash->entries);
+  hash->entries_length = new_hash->entries_length;
+  hash->table_capacity = new_hash->table_capacity;
+  hash->entries_capacity = new_hash->entries_capacity;
+  hash->table = new_hash->table;
+  hash->entries = new_hash->entries;
+}
+
+void SPerl_HASH_insert_norehash_long(SPerl* sperl, SPerl_HASH* hash, const char* key, int64_t length, int64_t value) {
+  (void)sperl;
+
+  assert(hash);
+  assert(key);
+  assert(length > 0);
+
+  int64_t hash_value = SPerl_HASH_FUNC_calc_hash(sperl, key, length);
+  int64_t index = hash_value % hash->table_capacity;
+
+  if (hash->table[index]) {
+    SPerl_HASH_ENTRY* next_entry = hash->table[index];
+    while (1) {
+      if (strncmp(next_entry->key, key, length) == 0) {
+        *(int64_t*)&next_entry->value = value;
+        break;
+      }
+      else {
+        if (next_entry->next_index != -1) {
+          next_entry = &hash->entries[next_entry->next_index];
+        }
+        else {
+          int64_t new_entry_index = SPerl_HASH_new_hash_entry_long(sperl, hash, key, value);
+          next_entry->next_index = new_entry_index;
+          break;
+        }
+      }
+    }
+  }
+  else {
+    int64_t new_entry_index = SPerl_HASH_new_hash_entry_long(sperl, hash, key, value);
+    hash->table[index] = &hash->entries[new_entry_index];
+  }
+}
+
+void SPerl_HASH_insert_long(SPerl* sperl, SPerl_HASH* hash, const char* key, int64_t length, int64_t value) {
+  (void)sperl;
+
+  assert(hash);
+  assert(key);
+  assert(length > 0);
+
+  // Rehash
+  if (hash->entries_length > hash->table_capacity * 0.75) {
+    int64_t new_table_capacity = (hash->table_capacity * 2) + 1;
+
+    SPerl_HASH_rehash_long(sperl, hash, new_table_capacity);
+  }
+
+  SPerl_HASH_insert_norehash_long(sperl, hash, key, length, value);
+}
+
+int64_t SPerl_HASH_search_long(SPerl* sperl, SPerl_HASH* hash, const char* key, int64_t length) {
+  (void)sperl;
+
+  assert(hash);
+  assert(key);
+  assert(length > 0);
+
+  int64_t hash_value = SPerl_HASH_FUNC_calc_hash(sperl, key, length);
+  int64_t index = hash_value % hash->table_capacity;
+
+  SPerl_HASH_ENTRY* next_entry = hash->table[index];
+  while (1) {
+    if (next_entry) {
+      if (strncmp(key, next_entry->key, length) == 0) {
+        return *(int64_t*)&next_entry->value;
       }
       else {
         if (next_entry->next_index == -1) {
