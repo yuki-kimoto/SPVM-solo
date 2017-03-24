cc159af54cf759465ca718491064b26507ce3f80
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-03-24 15:03:57 +0900

    cleanup allocator

--- sperl_allocator_util.c
+++ sperl_allocator_util.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "sperl_allocator_util.h"
 
@@ -11,54 +12,46 @@ void SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure() {
 }
 
 void* SPerl_ALLOCATOR_UTIL_safe_malloc(int64_t count, int64_t size) {
-  if (count > SIZE_MAX / size) {
+
+  if (count <= 0 || size <= 0 || count > INT64_MAX / size) {
     SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
   }
-
-  int64_t const allocation_size = count * size;
-  if (allocation_size == 0) {
-    SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
-  }
-
-  void* const block = malloc((size_t)allocation_size);
+  
+  int64_t allocation_size = count * size;
+  
+  void* block = malloc(allocation_size);
+  
   if (!block) {
     SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
   }
-
+  
   return block;
 }
 
 void* SPerl_ALLOCATOR_UTIL_safe_malloc_zero(int64_t count, int64_t size) {
-  if (count > SIZE_MAX / size) {
+  if (count <= 0 || size <= 0 || count > INT64_MAX / size) {
     SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
   }
-
-  int64_t const allocation_size = count * size;
-  if (allocation_size == 0) {
-    SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
-  }
-
-  void* const block = calloc(count, size);
+  
+  void* block = calloc(count, size);
+  
   if (!block) {
     SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
   }
-
+  
   return block;
 }
 
 void* SPerl_ALLOCATOR_UTIL_safe_realloc(void* ptr, int64_t count, int64_t size) {
-  if (count > SIZE_MAX / size) {
+  if (count <= 0 || size <= 0 || count > INT64_MAX / size) {
     SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
   }
 
-  int64_t const allocation_size = count * size;
-  if (allocation_size == 0) {
-    SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
-  }
+  int64_t allocation_size = count * size;
 
-  void* const block = realloc(ptr, allocation_size);
+  void* block = realloc(ptr, allocation_size);
+  
   if (!block) {
-    free(ptr);
     SPerl_ALLOCATOR_UTIL_exit_with_malloc_failure();
   }
 
