337c8505e1894d918340e776ff309f1b4452b90f
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-03-24 11:04:31 +0900

    add assert

--- sperl_hash_func.c
+++ sperl_hash_func.c
@@ -1,11 +1,14 @@
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "sperl.h"
 #include "sperl_hash_func.h"
 
 int64_t SPerl_HASH_FUNC_calc_hash(SPerl* sperl, const char* str, int64_t len) {
   (void)sperl;
+  
+  assert(len > 0);
   
   const char* str_tmp = str;
   uint32_t hash = 5381;
