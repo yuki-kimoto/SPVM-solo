bac811305972a31015d7ae4980ff54c741e6b420
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-04-05 10:32:54 +0900

    add allocator_runtime.c allocator_runtime.h

---
+++ sperl_allocator_runtime.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "sperl.h"
+#include "sperl_allocator_runtime.h"
---
+++ sperl_allocator_runtime.h
@@ -0,0 +1,6 @@
+#ifndef SPERL_ALLOCATOR_RUNTIME_H
+#define SPERL_ALLOCATOR_RUNTIME_H
+#include <stddef.h>
+#include "sperl_base.h"
+
+#endif
