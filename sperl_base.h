f5082cc9b3a47bb1f964a2174c2b504a0687701b
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-04-12 10:11:47 +0900

    add SPerl_VALUE_T

--- sperl_base.h
+++ sperl_base.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#ifndef SPerl_VALUE_T
+#  define SPerl_VALUE_T int64_t
+#endif
+
 /* sperl_env.h */
 struct SPerl_env;
 typedef struct SPerl_env SPerl_ENV;
