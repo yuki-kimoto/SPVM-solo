280805e65debbdf98ce453d424a505e343ad4aed
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-04-12 10:48:59 +0900

    cleanup

--- sperl_api.h
+++ sperl_api.h
@@ -19,7 +19,7 @@ int32_t SPerl_API_get_var_int(SPerl* sperl, int64_t index);
 int64_t SPerl_API_get_var_long(SPerl* sperl, int64_t index);
 float SPerl_API_get_var_float(SPerl* sperl, int64_t index);
 double SPerl_API_get_var_double(SPerl* sperl, int64_t index);
-intptr_t SPerl_API_get_var(SPerl* sperl, int64_t index);
+void* SPerl_API_get_var_ref(SPerl* sperl, int64_t index);
 
 void SPerl_API_push_var_byte(SPerl* sperl, int8_t value);
 void SPerl_API_push_var_short(SPerl* sperl, int16_t value);
@@ -27,7 +27,7 @@ void SPerl_API_push_var_int(SPerl* sperl, int32_t value);
 void SPerl_API_push_var_long(SPerl* sperl, int64_t value);
 void SPerl_API_push_var_float(SPerl* sperl, float value);
 void SPerl_API_push_var_double(SPerl* sperl, double value);
-void SPerl_API_push_var(SPerl* sperl, intptr_t value);
+void SPerl_API_push_var_ref(SPerl* sperl, void* value);
 
 void SPerl_API_push_ret_byte(SPerl* sperl, int8_t value);
 void SPerl_API_push_ret_short(SPerl* sperl, int16_t value);
@@ -35,7 +35,7 @@ void SPerl_API_push_ret_int(SPerl* sperl, int32_t value);
 void SPerl_API_push_ret_long(SPerl* sperl, int64_t value);
 void SPerl_API_push_ret_float(SPerl* sperl, float value);
 void SPerl_API_push_ret_double(SPerl* sperl, double value);
-void SPerl_API_push_ret(SPerl* sperl, intptr_t value);
+void SPerl_API_push_ret_ref(SPerl* sperl, void* value);
 
 int8_t SPerl_API_pop_ret_byte(SPerl* sperl);
 int16_t SPerl_API_pop_ret_short(SPerl* sperl);
@@ -43,6 +43,6 @@ int32_t SPerl_API_pop_ret_int(SPerl* sperl);
 int64_t SPerl_API_pop_ret_long(SPerl* sperl);
 float SPerl_API_pop_ret_float(SPerl* sperl);
 double SPerl_API_pop_ret_double(SPerl* sperl);
-intptr_t SPerl_API_pop_ret(SPerl* sperl);
+void* SPerl_API_pop_ret(SPerl* sperl);
 
 #endif
