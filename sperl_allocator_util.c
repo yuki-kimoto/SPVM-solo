8b9d4fbf6fea41098bb4cdf225f5f7290e9de59a
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-04-13 10:13:38 +0900

    safe_malloc, safe_malloc argument type become int32_t

--- sperl_allocator_util.c
+++ sperl_allocator_util.c
@@ -5,13 +5,13 @@
 
 #include "sperl_allocator_util.h"
 
-void* SPerl_ALLOCATOR_UTIL_safe_malloc(size_t count, size_t size) {
+void* SPerl_ALLOCATOR_UTIL_safe_malloc(int32_t count, int32_t size) {
   
   assert(count >= 0);
   assert(size >= 0);
-  assert(count <= SIZE_MAX / size);
+  assert(count <= INT32_MAX / size);
   
-  size_t allocation_size = count * size;
+  int32_t allocation_size = count * size;
   
   void* block = malloc(allocation_size);
   
@@ -23,18 +23,18 @@ void* SPerl_ALLOCATOR_UTIL_safe_malloc(size_t count, size_t size) {
   return block;
 }
 
-void* SPerl_ALLOCATOR_UTIL_safe_realloc(void* ptr, size_t count, size_t size) {
+void* SPerl_ALLOCATOR_UTIL_safe_realloc(void* ptr, int32_t count, int32_t size) {
 
   assert(count >= 0);
   assert(size >= 0);
-  assert(count <= SIZE_MAX / size);
+  assert(count <= INT32_MAX / size);
 
-  size_t allocation_size = count * size;
+  int32_t allocation_size = count * size;
 
   void* block = realloc(ptr, allocation_size);
   
   if (!block) {
-    fprintf(stderr, "Failed to allocate memory. Sperl will exit.\n");
+    fprintf(stderr, "Failed to reallocate memory. Sperl will exit.\n");
     exit(EXIT_FAILURE);
   }
 
--- sperl_allocator_util.h
+++ sperl_allocator_util.h
@@ -1,7 +1,7 @@
 #ifndef SPERL_ALLOCATOR_UTIL_H
 #define SPERL_ALLOCATOR_UTIL_H
 
-void* SPerl_ALLOCATOR_UTIL_safe_malloc(size_t count, size_t size);
-void* SPerl_ALLOCATOR_UTIL_safe_realloc(void* ptr, size_t count, size_t size);
+void* SPerl_ALLOCATOR_UTIL_safe_malloc(int32_t count, int32_t size);
+void* SPerl_ALLOCATOR_UTIL_safe_realloc(void* ptr, int32_t count, int32_t size);
 
 #endif
