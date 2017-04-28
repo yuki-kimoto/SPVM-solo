400671af7d2af1d2cf2e5ed88ac54e7856f49010
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-04-28 13:17:54 +0900

    fix constant

--- sperl_op.c
+++ sperl_op.c
@@ -123,7 +123,7 @@ SPerl_OP* SPerl_OP_new_op_constant_int(SPerl* sperl, int32_t value, const char* 
   SPerl_CONSTANT* constant = SPerl_CONSTANT_new(sperl);
   
   constant->code = SPerl_CONSTANT_C_CODE_INT;
-  constant->uv.int_value = 0;
+  constant->uv.int_value = value;
   constant->resolved_type = SPerl_HASH_search(sperl, sperl->parser->resolved_type_symtable, "int", strlen("int"));
   
   op_constant->uv.constant = constant;
@@ -136,7 +136,7 @@ SPerl_OP* SPerl_OP_new_op_constant_long(SPerl* sperl, int64_t value, const char*
   SPerl_CONSTANT* constant = SPerl_CONSTANT_new(sperl);
   
   constant->code = SPerl_CONSTANT_C_CODE_LONG;
-  constant->uv.long_value = 0;
+  constant->uv.long_value = value;
   constant->resolved_type = SPerl_HASH_search(sperl, sperl->parser->resolved_type_symtable, "long", strlen("long"));
   
   op_constant->uv.constant = constant;
@@ -149,11 +149,11 @@ SPerl_OP* SPerl_OP_new_op_constant_float(SPerl* sperl, float value, const char* 
   SPerl_CONSTANT* constant = SPerl_CONSTANT_new(sperl);
   
   constant->code = SPerl_CONSTANT_C_CODE_FLOAT;
-  constant->uv.float_value = 0;
+  constant->uv.float_value = value;
   constant->resolved_type = SPerl_HASH_search(sperl, sperl->parser->resolved_type_symtable, "float", strlen("float"));
   
   op_constant->uv.constant = constant;
-
+  
   return op_constant;
 }
 
@@ -162,11 +162,11 @@ SPerl_OP* SPerl_OP_new_op_constant_double(SPerl* sperl, double value, const char
   SPerl_CONSTANT* constant = SPerl_CONSTANT_new(sperl);
   
   constant->code = SPerl_CONSTANT_C_CODE_DOUBLE;
-  constant->uv.double_value = 0;
+  constant->uv.double_value = value;
   constant->resolved_type = SPerl_HASH_search(sperl, sperl->parser->resolved_type_symtable, "double", strlen("double"));
   
   op_constant->uv.constant = constant;
-
+  
   return op_constant;
 }
 
