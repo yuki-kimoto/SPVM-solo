d9c5339a02d3e1fbce577afedf32328f31aeabe0
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-05-01 16:40:30 +0900

    add string type

--- sperl_toke.c
+++ sperl_toke.c
@@ -430,7 +430,7 @@ int SPerl_yylex(SPerl_YYSTYPE* yylvalp, SPerl* sperl) {
         SPerl_CONSTANT* constant = SPerl_CONSTANT_new(sperl);
         constant->code = SPerl_CONSTANT_C_CODE_STRING;
         constant->uv.string_value = str;
-        constant->resolved_type = SPerl_HASH_search(sperl, parser->resolved_type_symtable, "byte[]", strlen("byte[]"));
+        constant->resolved_type = SPerl_HASH_search(sperl, parser->resolved_type_symtable, "string", strlen("string"));
         op->uv.constant = constant;
         yylvalp->opval = (SPerl_OP*)op;
         
