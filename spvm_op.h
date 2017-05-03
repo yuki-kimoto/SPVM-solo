8f8ed4262ab35b7a75f4c6ee78fd3cc82f1ce1e2
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-05-03 14:38:42 +0900

    cleanup

--- spvm_op.h
+++ spvm_op.h
@@ -190,55 +190,37 @@ struct SPVM_op {
   _Bool lvalue;
 };
 
-SPVM_OP* SPVM_OP_build_return(SPVM* spvm, SPVM_OP* op_return, SPVM_OP* op_term);
-SPVM_OP* SPVM_OP_build_die(SPVM* spvm, SPVM_OP* op_die, SPVM_OP* op_term);
-
-SPVM_OP* SPVM_OP_new_op_constant_int(SPVM* spvm, int32_t value, const char* file, int32_t line);
-SPVM_OP* SPVM_OP_new_op_constant_long(SPVM* spvm, int64_t value, const char* file, int32_t line);
-SPVM_OP* SPVM_OP_new_op_constant_float(SPVM* spvm, float value, const char* file, int32_t line);
-SPVM_OP* SPVM_OP_new_op_constant_double(SPVM* spvm, double value, const char* file, int32_t line);
-
-SPVM_OP* SPVM_OP_new_op_var_from_op_my_var(SPVM* spvm, SPVM_OP* op_my_var);
 
 SPVM_OP* SPVM_OP_get_op_block_from_op_sub(SPVM* spvm, SPVM_OP* op_sub);
+SPVM_RESOLVED_TYPE* SPVM_OP_get_resolved_type(SPVM* spvm, SPVM_OP* op);
 
 void SPVM_OP_convert_to_op_constant_true(SPVM* spvm, SPVM_OP* op);
 void SPVM_OP_convert_to_op_constant_false(SPVM* spvm, SPVM_OP* op);
-
 void SPVM_OP_convert_not_to_if(SPVM* spvm, SPVM_OP* op);
 void SPVM_OP_convert_and_to_if(SPVM* spvm, SPVM_OP* op);
 void SPVM_OP_convert_or_to_if(SPVM* spvm, SPVM_OP* op);
 
-SPVM_OP* SPVM_OP_build_try_catch(SPVM* spvm, SPVM_OP* op_try, SPVM_OP* op_try_block, SPVM_OP* op_catch, SPVM_OP* op_my_var, SPVM_OP* op_catch_block);
-
-SPVM_OP* SPVM_OP_build_switch_statement(SPVM* spvm, SPVM_OP* op_switch, SPVM_OP* op_term, SPVM_OP* op_block);
-SPVM_OP* SPVM_OP_build_case_statement(SPVM* spvm, SPVM_OP* op_case, SPVM_OP* op_term);
-
-SPVM_OP* SPVM_OP_build_logical_op(SPVM* spvm, SPVM_OP* op_logical_op, SPVM_OP* op_first, SPVM_OP* op_last);
-
-SPVM_OP* SPVM_OP_build_for_statement(SPVM* spvm, SPVM_OP* op_for, SPVM_OP* op_term_loop_var, SPVM_OP* op_term_condition, SPVM_OP* op_term_next_value, SPVM_OP* op_block);
-
-SPVM_OP* SPVM_OP_build_while_statement(SPVM* spvm, SPVM_OP* op_while, SPVM_OP* op_term_condition, SPVM_OP* op_block);
-
-
-SPVM_OP* SPVM_OP_build_if_statement(SPVM* spvm, SPVM_OP* op_if, SPVM_OP* op_term, SPVM_OP* op_block, SPVM_OP* op_else_statement);
-
-SPVM_OP* SPVM_OP_build_array_length(SPVM* spvm, SPVM_OP* op_array_length, SPVM_OP* op_term);
-
-SPVM_OP* SPVM_OP_build_malloc_object(SPVM* spvm, SPVM_OP* op_malloc, SPVM_OP* op_type);
-
-SPVM_OP* SPVM_OP_build_array_init(SPVM* spvm, SPVM_OP* op_opt_terms);
 void SPVM_OP_resolve_type(SPVM* spvm, SPVM_TYPE* type, int32_t name_length);
 void SPVM_OP_check(SPVM* spvm);
+
 void SPVM_OP_resolve_sub_name(SPVM* spvm, SPVM_OP* op_package, SPVM_OP* op_name);
 void SPVM_OP_resolve_field_name(SPVM* spvm, SPVM_OP* op_name);
 
-SPVM_RESOLVED_TYPE* SPVM_OP_get_resolved_type(SPVM* spvm, SPVM_OP* op);
+SPVM_OP* SPVM_OP_build_return(SPVM* spvm, SPVM_OP* op_return, SPVM_OP* op_term);
+SPVM_OP* SPVM_OP_build_die(SPVM* spvm, SPVM_OP* op_die, SPVM_OP* op_term);
+SPVM_OP* SPVM_OP_build_try_catch(SPVM* spvm, SPVM_OP* op_try, SPVM_OP* op_try_block, SPVM_OP* op_catch, SPVM_OP* op_my_var, SPVM_OP* op_catch_block);
+SPVM_OP* SPVM_OP_build_switch_statement(SPVM* spvm, SPVM_OP* op_switch, SPVM_OP* op_term, SPVM_OP* op_block);
+SPVM_OP* SPVM_OP_build_case_statement(SPVM* spvm, SPVM_OP* op_case, SPVM_OP* op_term);
+SPVM_OP* SPVM_OP_build_logical_op(SPVM* spvm, SPVM_OP* op_logical_op, SPVM_OP* op_first, SPVM_OP* op_last);
+SPVM_OP* SPVM_OP_build_for_statement(SPVM* spvm, SPVM_OP* op_for, SPVM_OP* op_term_loop_var, SPVM_OP* op_term_condition, SPVM_OP* op_term_next_value, SPVM_OP* op_block);
+SPVM_OP* SPVM_OP_build_while_statement(SPVM* spvm, SPVM_OP* op_while, SPVM_OP* op_term_condition, SPVM_OP* op_block);
+SPVM_OP* SPVM_OP_build_if_statement(SPVM* spvm, SPVM_OP* op_if, SPVM_OP* op_term, SPVM_OP* op_block, SPVM_OP* op_else_statement);
+SPVM_OP* SPVM_OP_build_array_length(SPVM* spvm, SPVM_OP* op_array_length, SPVM_OP* op_term);
+SPVM_OP* SPVM_OP_build_malloc_object(SPVM* spvm, SPVM_OP* op_malloc, SPVM_OP* op_type);
+SPVM_OP* SPVM_OP_build_array_init(SPVM* spvm, SPVM_OP* op_opt_terms);
 SPVM_OP* SPVM_OP_build_binop(SPVM* spvm, SPVM_OP* op_call_op, SPVM_OP* op_first, SPVM_OP* op_last);
-
 SPVM_OP* SPVM_OP_build_type_name(SPVM* spvm, SPVM_OP* op_type_name);
 SPVM_OP* SPVM_OP_build_type_array(SPVM* spvm, SPVM_OP* op_type, SPVM_OP* op_term);
-
 SPVM_OP* SPVM_OP_build_call_field(SPVM* spvm, SPVM_OP* op_name_package, SPVM_OP* op_name_field);
 SPVM_OP* SPVM_OP_build_package(SPVM* spvm, SPVM_OP* op_package, SPVM_OP* op_name_package, SPVM_OP* op_block);
 SPVM_OP* SPVM_OP_build_sub(SPVM* spvm, SPVM_OP* op_sub, SPVM_OP* op_subname, SPVM_OP* op_subargs, SPVM_OP* op_descriptors, SPVM_OP* type, SPVM_OP* op_block);
@@ -248,27 +230,29 @@ SPVM_OP* SPVM_OP_build_my_var(SPVM* spvm, SPVM_OP* op_my, SPVM_OP* op_var, SPVM_
 SPVM_OP* SPVM_OP_build_grammar(SPVM* spvm, SPVM_OP* op_packages);
 SPVM_OP* SPVM_OP_build_use(SPVM* spvm, SPVM_OP* op_use, SPVM_OP* op_name_package);
 SPVM_OP* SPVM_OP_build_call_sub(SPVM* spvm, SPVM_OP* op_invocant, SPVM_OP* op_subname, SPVM_OP* op_terms);
-SPVM_OP* SPVM_OP_new_op_list(SPVM* spvm, const char* file, int32_t line);
 SPVM_OP* SPVM_OP_build_convert_type(SPVM* spvm, SPVM_OP* op_type, SPVM_OP* op_term);
+SPVM_OP* SPVM_OP_build_enumeration(SPVM* spvm, SPVM_OP* op_enumeration, SPVM_OP* op_enumeration_block);
+SPVM_OP* SPVM_OP_build_unop(SPVM* spvm, SPVM_OP* op_unary, SPVM_OP* op_first);
+SPVM_OP* SPVM_OP_build_array_elem(SPVM* spvm, SPVM_OP* op_var, SPVM_OP* op_term);
+
 void SPVM_OP_resolve_op_convert_type(SPVM* spvm, SPVM_OP* op_convert_type);
 
 const char* SPVM_OP_create_abs_name(SPVM* spvm, const char* package_name, const char* base_name);
 
-SPVM_OP* SPVM_OP_sibling_splice(SPVM* spvm, SPVM_OP* parent, SPVM_OP* start, int32_t del_count, SPVM_OP *insert);
+SPVM_OP* SPVM_OP_new_op_constant_int(SPVM* spvm, int32_t value, const char* file, int32_t line);
+SPVM_OP* SPVM_OP_new_op_constant_long(SPVM* spvm, int64_t value, const char* file, int32_t line);
+SPVM_OP* SPVM_OP_new_op_constant_float(SPVM* spvm, float value, const char* file, int32_t line);
+SPVM_OP* SPVM_OP_new_op_constant_double(SPVM* spvm, double value, const char* file, int32_t line);
+SPVM_OP* SPVM_OP_new_op_var_from_op_my_var(SPVM* spvm, SPVM_OP* op_my_var);
+SPVM_OP* SPVM_OP_new_op_list(SPVM* spvm, const char* file, int32_t line);
+SPVM_OP* SPVM_OP_new_op(SPVM* spvm, int32_t code, const char* file, int32_t line);
 
+SPVM_OP* SPVM_OP_sibling_splice(SPVM* spvm, SPVM_OP* parent, SPVM_OP* start, int32_t del_count, SPVM_OP *insert);
 SPVM_OP* SPVM_OP_sibling(SPVM* spvm, SPVM_OP* o);
 void SPVM_OP_moresib_set(SPVM* spvm, SPVM_OP* o, SPVM_OP* sib);
 void SPVM_OP_lastsib_set(SPVM* spvm, SPVM_OP* o, SPVM_OP* parent);
 void SPVM_OP_maybesib_set(SPVM* spvm, SPVM_OP* o, SPVM_OP* sib, SPVM_OP* parent);
-
-SPVM_OP* SPVM_OP_build_enumeration(SPVM* spvm, SPVM_OP* op_enumeration, SPVM_OP* op_enumeration_block);
-
-SPVM_OP* SPVM_OP_new_op(SPVM* spvm, int32_t code, const char* file, int32_t line);
-
 SPVM_OP* SPVM_OP_append_elem(SPVM* spvm, SPVM_OP *first, SPVM_OP *last, const char* file, int32_t line);
 
-SPVM_OP* SPVM_OP_build_unop(SPVM* spvm, SPVM_OP* op_unary, SPVM_OP* op_first);
-
-SPVM_OP* SPVM_OP_build_array_elem(SPVM* spvm, SPVM_OP* op_var, SPVM_OP* op_term);
 
 #endif
