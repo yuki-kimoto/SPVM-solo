91ade4db8d3335afb03ccc6375fae622732f4472
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-05-17 11:04:19 +0900

    use more SPVM_VALUE

--- spvm_env.c
+++ spvm_env.c
@@ -3,6 +3,7 @@
 #include "spvm.h"
 #include "spvm_env.h"
 #include "spvm_allocator_util.h"
+#include "spvm_value.h"
 
 SPVM_ENV* SPVM_ENV_new(SPVM* spvm) {
   (void)spvm;
@@ -10,7 +11,7 @@ SPVM_ENV* SPVM_ENV_new(SPVM* spvm) {
   SPVM_ENV* env = SPVM_ALLOCATOR_UTIL_safe_malloc_i32(1, sizeof(SPVM_ENV));
   
   env->call_stack_capacity = 0xFF;
-  env->call_stack = SPVM_ALLOCATOR_UTIL_safe_malloc_i32(env->call_stack_capacity, sizeof(intmax_t));
+  env->call_stack = SPVM_ALLOCATOR_UTIL_safe_malloc_i32(env->call_stack_capacity, sizeof(SPVM_VALUE));
   env->call_stack_base = -1;
   env->operand_stack_top = -1;
   env->abort = 0;
