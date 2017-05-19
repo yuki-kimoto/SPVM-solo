9104fad73da206b5ffb70332dfb55fec8c59d1ca
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-05-19 14:56:23 +0900

    remove I specifier

--- spvm_toke.c
+++ spvm_toke.c
@@ -493,11 +493,7 @@ int SPVM_yylex(SPVM_YYSTYPE* yylvalp, SPVM* spvm) {
           
           // Constant type
           int32_t constant_code;
-          if (num_str[str_len - 1] == 'I')  {
-            constant_code = SPVM_CONSTANT_C_CODE_INT;
-            num_str[str_len - 1] = '\0';
-          }
-          else if (num_str[str_len - 1] == 'L')  {
+          if (num_str[str_len - 1] == 'L')  {
             constant_code = SPVM_CONSTANT_C_CODE_LONG;
             num_str[str_len - 1] = '\0';
           }
