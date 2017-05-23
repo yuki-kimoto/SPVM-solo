aca04e824708acf946e3fe48a76a79f169bd9e14
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-05-23 09:28:44 +0900

    add ref_field_count to constant_pool_package

--- spvm_constant_pool_package.h
+++ spvm_constant_pool_package.h
@@ -5,10 +5,11 @@
 
 // Array
 struct SPVM_constant_pool_package {
+  SPVM_VALUE* free_need_field_offsets_pool_address;
   int32_t name_constant_pool_address;
   int32_t byte_size;
   int32_t free_need_field_offsets_length;
-  SPVM_VALUE* free_need_field_offsets_pool_address;
+  int32_t ref_field_count;
 };
 
 #endif
