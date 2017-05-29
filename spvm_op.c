4be605affd468498eabf74e58cc38a8302d6d569
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-05-29 15:46:15 +0900

    add extention_bind. extention_bind.c

--- spvm_op.c
+++ spvm_op.c
@@ -29,9 +29,10 @@
 #include "spvm_op_checker.h"
 #include "spvm_switch_info.h"
 #include "spvm_descriptor.h"
-#include "spvm_extention.h"
 #include "spvm_allocator_parser.h"
 #include "spvm_limit.h"
+#include "spvm_extention.h"
+#include "spvm_extention_bind.h"
 
 const char* const SPVM_OP_C_CODE_NAMES[] = {
   "IF",
@@ -920,36 +921,7 @@ SPVM_OP* SPVM_OP_build_package(SPVM* spvm, SPVM_OP* op_package, SPVM_OP* op_name
         else {
           // Bind standard functions
           if (sub->is_native) {
-            if (strcmp(sub_abs_name, "std::println_byte") == 0) {
-              sub->native_address = SPVM_EXTENTION_std__println_byte;
-            }
-            else if (strcmp(sub_abs_name, "std::println_short") == 0) {
-              sub->native_address = SPVM_EXTENTION_std__println_short;
-            }
-            else if (strcmp(sub_abs_name, "std::println_int") == 0) {
-              sub->native_address = SPVM_EXTENTION_std__println_int;
-            }
-            else if (strcmp(sub_abs_name, "std::println_long") == 0) {
-              sub->native_address = SPVM_EXTENTION_std__println_long;
-            }
-            else if (strcmp(sub_abs_name, "std::println_float") == 0) {
-              sub->native_address = SPVM_EXTENTION_std__println_float;
-            }
-            else if (strcmp(sub_abs_name, "std::println_double") == 0) {
-              sub->native_address = SPVM_EXTENTION_std__println_double;
-            }
-            else if (strcmp(sub_abs_name, "std::println") == 0) {
-              sub->native_address = SPVM_EXTENTION_std__println;
-            }
-            else if (strcmp(sub_abs_name, "std::sum_int") == 0) {
-              sub->native_address = SPVM_EXTENTION_std__sum_int;
-            }
-            else if (strcmp(sub_abs_name, "std::test_call1") == 0) {
-              sub->native_address = SPVM_EXTENTION_std__test_call1;
-            }
-            else if (strcmp(sub_abs_name, "std::test_call2") == 0) {
-              sub->native_address = SPVM_EXTENTION_std__test_call2;
-            }
+            SPVM_EXTENTION_BIND_bind_core_extentions(spvm, sub, sub_abs_name);
           }
           
           sub->abs_name = sub_abs_name;
