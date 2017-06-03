0f3fc7b9dc882ebe4cc533861a97042bfa678159
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-06-03 10:55:40 +0900

    cleanup env

--- spvm_env.c
+++ spvm_env.c
@@ -5,6 +5,14 @@
 #include "spvm_util_allocator.h"
 #include "spvm_value.h"
 
+SPVM_ENV* SPVM_ENV_init(SPVM* spvm, SPVM_ENV* env) {
+  (void)spvm;
+  
+  env->call_stack_base = -1;
+  env->operand_stack_top = -1;
+  env->abort = 0;
+}
+
 SPVM_ENV* SPVM_ENV_new(SPVM* spvm) {
   (void)spvm;
   
@@ -12,9 +20,8 @@ SPVM_ENV* SPVM_ENV_new(SPVM* spvm) {
   
   env->call_stack_capacity = 0xFF;
   env->call_stack = SPVM_UTIL_ALLOCATOR_safe_malloc_i32(env->call_stack_capacity, sizeof(SPVM_VALUE));
-  env->call_stack_base = -1;
-  env->operand_stack_top = -1;
-  env->abort = 0;
+  
+  SPVM_ENV_init(spvm, env);
   
   return env;
 }
