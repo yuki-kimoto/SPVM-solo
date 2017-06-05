2e79ae1c7c7da6c87cc0354761b4162c5d3080aa
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-06-05 17:26:38 +0900

    allow multi array declaration

--- spvm_constant_pool_package.h
+++ spvm_constant_pool_package.h
@@ -8,7 +8,6 @@ struct SPVM_constant_pool_package {
   SPVM_VALUE* free_need_field_offsets_pool_address;
   int32_t name_constant_pool_address;
   int32_t byte_size;
-  int32_t free_need_field_offsets_length;
   int32_t data_fields_count;
 };
 
